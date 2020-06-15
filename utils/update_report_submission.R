4b21d22f1d83d573f1b1e9eca2aee8e5514ae793
sophiemeakin <sophiemeakin@outlook.com>
2020-06-15 17:47:51 +0100

    First forecast plus small changes
    * forecast for 2020-06-15
    * some updates to the submission formatting script

--- utils/update_report_submission.R
+++ utils/update_report_submission.R
@@ -12,17 +12,17 @@ forecast_dir <- here::here("rt-forecast")  # Assumes forecasts are in national a
 
 # Find forecasts ----------------------------------------------------------
 
-forecasts <- c(list.dirs(file.path(forecast_dir, "regional"), recursive = FALSE),
+forecasts <- c(list.dirs(file.path(forecast_dir, "state"), recursive = FALSE),
                list.dirs(file.path(forecast_dir, "national"), recursive = FALSE))
 names(forecasts) <- forecasts %>% 
-  stringr::str_remove(file.path(forecast_dir, "regional/")) %>% 
+  stringr::str_remove(file.path(forecast_dir, "state/")) %>% 
   stringr::str_remove(file.path(forecast_dir, "national/"))
 
 # Extract forecasts -------------------------------------------------------
 
 source(here::here("utils/format_submission.R"))
 
-region_forecasts <- purrr::map2_dfr(forecasts, names(forecasts),
+region_forecasts <- purrr::map2_dfr(.x = forecasts, .y = names(forecasts),
                                     ~ format_forecast(loc = .x, loc_name = .y, 
                                                       forecast_date = forecast_date,
                                                       forecast_adjustment = 5 + 9))
