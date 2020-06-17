0823dfd0c2d58675a35ca0aab052714105d3696c
nikosbosse <nikosbosse@gmail.com>
2020-06-17 16:26:10 +0200

    hacked together a function to run past forecasts

--- run_everything.R
+++ run_everything.R
@@ -2,64 +2,54 @@
 # ============================================================================ #
 # Update data used for the forecasts
 # ============================================================================ #
-
 source("utils/get_us_deaths.R")
 
-# ============================================================================ #
-# Run all models
-# ============================================================================ #
 
-# ----------- #
-# Rt forecast #
-# ----------- #
+
+# ============================================================================ #
+# load in function to prepare submissions
+# ============================================================================ #
+source("utils/format_submission.R")
+
+
+
 
 # update delays (not run)
 # source("rt-forecast/update-delays.R")
 
-# make new rt forecasts for US deaths
-source("rt-forecast/update-us-deaths-rt.R")
-
-
-# --------------- #
-# direct forecast #
-# --------------- #
-
-# work in progress
-
-
 
 # ============================================================================ #
-# Prepare submission format
+# Run all models, format submission, do plotting
 # ============================================================================ #
 
-# load in function
-source("utils/format_submission.R")
 
-# run formatting function
-# results are stored in death-forecast/epiforecasts-ensemble1
-source("utils/update_report_submission.R")
-source("utils/format_submission.R")
+target_dates <- seq(ymd('2020-05-15'), ymd(Sys.Date()), by = '1 day')
+
+for (t in target_dates) {
+  target_date <- t
+  # make new rt forecasts for US deaths
+  source(here::here("rt-forecast", "update-us-deaths-rt.R"))
+  
+  # format submission
+  forecast_date <- t
+  source(here::here("utils", "update_report_submission.R"))
+  
+  dir.create(here::here("visualisation", t), showWarnings = FALSE)
+  source(here::here("utils", "visualise_submission.R"))
+  
+  # visualise national death forecast
+  national <- plot_forecasts(national = TRUE, states = NULL, cutoff = 25) + 
+    theme(text = element_text(family = "Sans Serif"))
+  
+  ggsave(plot = national, path = here::here("visualisation", t), 
+         filename = "death_national.png")
+  
+  # visualise subnational death forecast
+  subnational <- plot_forecasts(national = FALSE, states = NULL, cutoff = 25) + 
+    theme(text = element_text(family = "Sans Serif"))
+  
+  ggsave(plot = subnational, path = here::here("visualisation", t), 
+         filename = "death_subnational.png", height = 30, width = 30)
+}
 
 
-
-# ============================================================================ #
-# Visualise submissions
-# ============================================================================ #
-
-date <- Sys.Date()
-dir.create(here::here("visualisation", date))
-source("utils/visualise_submission.R")
-
-# visualise national death forecast
-national <- plot_forecasts(national = TRUE, states = NULL, cutoff = 25) + 
-  theme(text = element_text(family = "Sans Serif"))
-
-ggsave(plot = national, path = here::here("visualisation", date), 
-       filename = "death_national.png")
-
-# visualise subnational death forecast
-subnational <- plot_forecasts(national = FALSE, states = NULL, cutoff = 25) + 
-  theme(text = element_text(family = "Sans Serif"))
-
-ggsave(plot = subnational, path = here::here("visualisation", date), 
-       filename = "death_subnational.png", height = 30, width = 30)
