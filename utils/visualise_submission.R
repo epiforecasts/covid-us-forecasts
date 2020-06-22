884eb990ada9a57aa47c35f49f6a3ad82ca2fce5
sophiemeakin <sophiemeakin@outlook.com>
2020-06-22 15:44:40 +0100

    Update forecast visualisation to include time series models

--- utils/visualise_submission.R
+++ utils/visualise_submission.R
@@ -1,9 +1,12 @@
 library(tidyverse)
 
-plot_forecasts = function(national = TRUE, states = NULL, cutoff = 25){
+plot_forecasts = function(national = TRUE, states = NULL, forecast_date = Sys.Date(), cutoff = 25){
   
+  forecast_date <- lubridate::floor_date(forecast_date, unit = "week", week_start = 1)
+
   ## Load current observed deaths data
-  deaths_data <- readRDS(here::here("rt-forecast/data/deaths_data.rds")) %>%
+  source("utils/get_us_deaths.R")
+  deaths_data <- readRDS(here::here("data/deaths_data.rds")) %>%
     dplyr::mutate(week = lubridate::floor_date(date, unit = "week", week_start = 7)) %>%
     dplyr::group_by(state, week) %>%
     dplyr::summarise(week_deaths = sum(deaths, na.rm = TRUE))
@@ -15,7 +18,7 @@ plot_forecasts = function(national = TRUE, states = NULL, cutoff = 25){
                 dplyr::mutate(state = "US")) %>%
     select(week_beginning = week, state, value = week_deaths) %>%
     mutate(type = "observed_data") %>%
-    filter(week_beginning < max(week_beginning),
+    filter(week_beginning < forecast_date-1,
            week_beginning >= as.Date("2020-05-01"))
   
   high_states <- deaths_data %>%
@@ -24,8 +27,9 @@ plot_forecasts = function(national = TRUE, states = NULL, cutoff = 25){
            state != "US") %>%
     .$state
   
-  ## Load most recent forecast data
-  forecast_data <- read.csv(here::here("death-forecast/epiforecasts-ensemble1/2020-06-15-epiforecasts-ensemble1.csv")) %>%
+  ## Load most recent Rt forecast data
+  forecast_file <- paste0("death-forecast/epiforecasts-ensemble1/", forecast_date, "-epiforecasts-ensemble1.csv")
+  forecast_data <- read.csv(here::here(forecast_file)) %>%
     tibble() %>%
     mutate(week = as.Date(target_end_date) - 6) %>%
     left_join(tigris::fips_codes %>%
@@ -33,18 +37,36 @@ plot_forecasts = function(national = TRUE, states = NULL, cutoff = 25){
                 unique() %>%
                 rbind(c("US", "US")),
               by = c("location" = "state_code")) %>%
-    select(week_beginning = week, state = state_name, target, type, quantile, value)
+    select(week_beginning = week, state = state_name, target, type, quantile, value) %>%
+    mutate(model = "rt_forecast")
   forecast_states = unique(forecast_data$state)
+  
+  ## Load most recent deaths time-series forecast
+  # ts_deaths_file <- paste0("timeseries-forecast/deaths-only/", "2020-06-22", ".rds")
+  ts_deaths_file <- "https://raw.githubusercontent.com/epiforecasts/covid-us-forecasts/timeseries/timeseries-forecast/deaths-only/latest-weekly-inc-deaths-only.csv"
+  ts_deaths <- read.csv(ts_deaths_file) %>%
+    select(week_beginning = week, state, type = model_type, quantile, value = deaths) %>%
+    mutate(week_beginning = as.Date(week_beginning),
+           model = "timeseries_deaths_only",
+           type = "quantile")
+  ts_cases_file <- "https://raw.githubusercontent.com/epiforecasts/covid-us-forecasts/timeseries/timeseries-forecast/deaths-on-cases/latest-weekly-inc-deaths-on-cases.csv"
+  ts_cases <- read.csv(ts_cases_file) %>%
+    select(week_beginning = week, state, type = model_type, quantile, value = deaths) %>%
+    mutate(week_beginning = as.Date(week_beginning),
+           model = "timeseries_deaths_cases",
+           type = "quantile")
   
   
   # Combine and reshape for plotting
   plotting_data <- forecast_data %>%
     bind_rows(deaths_data %>% filter(state %in% forecast_states)) %>%
+    bind_rows(ts_deaths %>% filter(state %in% forecast_states)) %>%
+    bind_rows(ts_cases %>% filter(state %in% forecast_states)) %>%
     filter(grepl("inc", target) | is.na(target)) %>%
     select(-target) %>%
     mutate(q_type = ifelse(type %in% c("point", "observed_data"), type, paste0(type, quantile))) %>%
-    select(week_beginning, state, q_type, value) %>%
-    pivot_wider(id_cols = c(week_beginning, state), names_from = q_type, values_from = value)
+    select(week_beginning, state, model, q_type, value) %>%
+    pivot_wider(id_cols = c(week_beginning, state, model), names_from = q_type, values_from = value)
   
   ##
   if (national) {
@@ -68,14 +90,18 @@ plot_forecasts = function(national = TRUE, states = NULL, cutoff = 25){
   
   ## Plotting!
   plotting_data %>%
-    ggplot(aes(x = week_beginning)) +
-    geom_line(aes(y = observed_data), lwd = 1) +
-    geom_point(aes(y = point), size = 1, col = "red") +
-    geom_line(aes(y = point), lwd = 1, col = "red") +
-    # geom_ribbon(aes(ymin = quantile0.05, ymax = quantile0.95), fill = "red", alpha = 0.2) +
-    geom_ribbon(aes(ymin = quantile0.25, ymax = quantile0.75), fill = "red", alpha = 0.2) +
+    ggplot(aes(x = week_beginning,  col = model, fill = model)) +
+    ## Observed data
+    geom_point(aes(y = observed_data), size = 2, col = "black") +
+    geom_line(aes(y = observed_data), lwd = 1, col = "black") +
+    ## Forecasts (50% CI)
+    geom_point(aes(y = quantile0.5), size = 2) +
+    geom_line(aes(y = quantile0.5), lwd = 1) +
+    geom_ribbon(aes(ymin = quantile0.25, ymax = quantile0.75), color = NA, alpha = 0.2) +
+    ##
     facet_wrap(.~ state, scales = "free_y") +
     labs(x = "Week beginning", y = "Weekly incident deaths") +
-    cowplot::theme_cowplot()
+    cowplot::theme_cowplot() +
+    theme(legend.position = "top")
   
 }
