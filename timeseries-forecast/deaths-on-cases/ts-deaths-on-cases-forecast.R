09bf5bd912a1c68bb95d79ddbd0e7eb8a9420091
kathsherratt <katharine.sherratt@lshtm.ac.uk>
2020-06-26 15:37:52 +0100

    bug-fixing and updating
    - Some bugs found and removed
    - Mainly caused by transferring over from daily to weekly data
    - Also a problem with not grouping deaths by state in deaths-only forecast (!)
    - Added option to specify weeks of right-truncation
    - Still need to fix bugs in later formatting before ensemble

--- timeseries-forecast/deaths-on-cases/ts-deaths-on-cases-forecast.R
+++ timeseries-forecast/deaths-on-cases/ts-deaths-on-cases-forecast.R
@@ -19,68 +19,89 @@
 library(magrittr); library(dplyr); library(tidyr); library(EpiSoon); library(forecastHybrid)
 
 ts_deaths_on_cases_forecast <- function(case_data, deaths_data, case_quantile,
-                                           sample_count, horizon_weeks,
+                                           sample_count, horizon_weeks, right_truncate_weeks,
                                            format = FALSE, quantiles_out = NULL){
     
 
 # Forecast cases ----------------------------------------------------------
 
-  case_data_weekly <- case_data %>%
-    mutate(week = as.Date(lubridate::floor_date(date, unit = "week", week_start = 6))) %>%
+  # Set up case data and truncate
+  case_data_weekly_full <- case_data %>%
+    mutate(week = as.Date(lubridate::floor_date(date, unit = "week", week_start = 6)),
+           date = NULL) %>%
     group_by(state, week) %>%
     summarise(cases = sum(cases)) 
+
   
+  right_truncate_date <- max(case_data_weekly_full$week) - 7 * right_truncate_weeks
   
-  y = case_data_weekly$cases
+  case_data_weekly <- case_data_weekly_full %>%
+    filter(week <= right_truncate_date)
   
+  # Set up dates
+  # Forecasting = next week + horizon_weeks
+  forecast_weeks <- seq.Date(from = as.Date(max(case_data_weekly$week)+7), by = 7, length.out = horizon_weeks)
   
-    # Forecast
-    case_forecast <- case_data %>%
+  # Historical = last 6 weeks of data
+  historical_weeks <- case_data_weekly %>%
+    ungroup() %>%
+    filter(!week %in% forecast_weeks) %>%
+    select(week) %>%
+    filter(week > max(week) - 42) %>%
+    unique() %>%
+    pull(week)
+  
+  # Forecast y
+    case_forecast <- case_data_weekly %>%
       group_by(state) %>%
-      group_modify(~ EpiSoon::forecastHybrid_model(y = y[max(1, length(y) - 6):length(y)],
+      group_modify(~ EpiSoon::forecastHybrid_model(y = filter(.x, week %in% historical_weeks) %>% pull("cases"),
                                                    samples = sample_count, 
                                                    horizon = horizon_weeks,
                                                    model_params = list(models = "aefz", weights = "equal"),
                                                    forecast_params = list(PI.combination = "mean"))) %>%
       mutate(sample = rep(1:sample_count)) %>%
-      pivot_longer(cols = starts_with("..."), names_to = "date")
+      pivot_longer(cols = starts_with("..."), names_to = "week")
     
     # Get quantile
       quantile <- case_forecast %>%
-        group_by(state, date) %>%
-        group_modify( ~ as.data.frame(quantile(.x$value, probs = case_quantile))) %>%
+        group_by(state, week) %>%
+        group_modify( ~ as.data.frame(quantile(.x$value, probs = case_quantile, na.rm = T))) %>%
         mutate(quantile = case_quantile) %>%
         ungroup()
       
       # Set up dates
-      dates_from <- unique(quantile$date)
+      dates_from <- unique(quantile$week)
       forecast_weeks <- seq.Date(from = as.Date(max(case_data_weekly$week)+7), by = 7, length.out = horizon_weeks)
       historical_weeks <- filter(case_data_weekly, !week %in% forecast_weeks) %>% pull(week) %>% unique()
       
       # Format
       case_forecast <- quantile %>%
-        select(state, date, "cases" = 3) %>%
-        mutate(week = recode(date, !!! setNames(forecast_weeks, dates_from)),
+        select(state, week, "cases" = 3) %>%
+        mutate(week = recode(week, !!! setNames(forecast_weeks, dates_from)),
                cases = ifelse(cases < 0, 0, cases),
-               cases = round(cases),
-               date = NULL)
+               cases = round(cases))
 
       
 
 # Forecast deaths ---------------------------------------------------------
 
-      # Join deaths and cases
-      deaths_data_weekly <- deaths_data %>%
+      
+      # Set deaths data and truncate
+      deaths_data_weekly_full <- deaths_data %>%
         mutate(week = as.Date(lubridate::floor_date(date, unit = "week", week_start = 6)),
                date = NULL) %>%
         group_by(state, week) %>%
         summarise(deaths = sum(deaths))
       
+      deaths_data_weekly <- deaths_data_weekly_full %>%
+        filter(week <= right_truncate_date)
+      
+      # Join deaths and cases
       cases_deaths <- dplyr::bind_rows(case_data_weekly, case_forecast) %>%
         full_join(deaths_data_weekly, by = c("state", "week")) %>%
         mutate(week = lubridate::ymd(week))
       
-      # Forecast deaths using cases
+      # Forecast deaths (y) using cases
       death_forecast <- cases_deaths %>%
         group_by(state) %>%
         group_modify(~ EpiSoon::forecastHybrid_model(y = filter(.x, week %in% historical_weeks) %>% pull("deaths"),
@@ -94,7 +115,7 @@ ts_deaths_on_cases_forecast <- function(case_data, deaths_data, case_quantile,
                                                                             PI.combination = "mean")
                                                      )) %>%
         mutate(sample = rep(1:sample_count)) %>%
-        pivot_longer(cols = starts_with("..."), names_to = "date")
+        pivot_longer(cols = starts_with("..."), names_to = "week")
       
       
 
@@ -107,12 +128,12 @@ ts_deaths_on_cases_forecast <- function(case_data, deaths_data, case_quantile,
       if(format == TRUE){
         # Get quantiles
         quantile <- death_forecast %>%
-          group_by(state, date) %>%
-          group_modify( ~ as.data.frame(quantile(.x$value, probs = quantiles_out))) %>%
+          group_by(state, week) %>%
+          group_modify( ~ as.data.frame(quantile(.x$value, probs = quantiles_out, na.rm = T))) %>%
           mutate(quantile = quantiles_out) %>%
           ungroup() %>%
-          select(state, date, quantile, "deaths" = 3) %>%
-          mutate(date = recode(date, !!! setNames(forecast_weeks, dates_from)),
+          select(state, week, quantile, "deaths" = 3) %>%
+          mutate(week = recode(week, !!! setNames(forecast_weeks, dates_from)),
                  deaths = ifelse(deaths < 0, 0, deaths),
                  deaths = round(deaths),
                  model_type = "deaths_on_cases",
--- timeseries-forecast/deaths-only/ts-deaths-only-forecast.R
+++ timeseries-forecast/deaths-only/ts-deaths-only-forecast.R
@@ -7,34 +7,52 @@ library(magrittr); library(dplyr); library(tidyr); library(EpiSoon); library(for
 # 
 # Arguments
 # data : dataframe - daily data including date, state, and death count
-# horizon_weeks : numeric - number of days to forecast over
+# horizon_weeks : numeric - number of weeks to forecast over
+# right_truncate_weeks : numeric - number of weeks (epiweeks) to remove from most recent date in data
 # format : logical - to return forecast formatted in quantiles appropriate for US forecast submission
 # quantiles_out : numeric - if format == TRUE, vector of quantile probabilities to return
 
 # Function to forecast ----------------------------------------------------
 
 ts_deaths_only_forecast <- function(data, 
-                                      sample_count, horizon_weeks, 
+                                      sample_count, horizon_weeks, right_truncate_weeks,
                                       format = FALSE, quantiles_out = NULL){
   
-  data_weekly <- data %>%
-    mutate(week = as.Date(lubridate::floor_date(date, unit = "week", week_start = 6))) %>%
+  # Set up data and truncate
+  data_weekly_full <- data %>%
+    mutate(week = as.Date(lubridate::floor_date(date, unit = "week", week_start = 6)),
+           date = NULL) %>%
     group_by(state, week) %>%
     summarise(deaths = sum(deaths))
     
+  right_truncate_date <- max(data_weekly_full$week) - 7 * right_truncate_weeks
   
-  y = data_weekly$deaths
+  data_weekly <- data_weekly_full %>%
+    filter(week <= right_truncate_date)
   
-  # Forecast
+  # Set up forecasting vs historical dates
+  # Forecasting = next week + horizon_weeks
+  forecast_weeks <- seq.Date(from = as.Date(max(data_weekly$week)+7), by = 7, length.out = horizon_weeks)
+  # Historical = last 6 weeks of data
+  historical_weeks <- data_weekly %>%
+    ungroup() %>%
+    filter(!week %in% forecast_weeks) %>%
+    select(week) %>%
+    filter(week > max(week) - 42) %>%
+    unique() %>%
+    pull(week)
+  
+  
+  # Forecast 
   death_forecast <- data_weekly %>%
     group_by(state) %>%
-    group_modify(~ EpiSoon::forecastHybrid_model(y = y[max(1, length(y) - 6):length(y)],
+    group_modify(~ EpiSoon::forecastHybrid_model(y = filter(.x, week %in% historical_weeks) %>% pull("deaths"),
                                                  samples = sample_count, 
                                                  horizon = horizon_weeks,
                                                  model_params = list(models = "aefz", weights = "equal"),
                                                  forecast_params = list(PI.combination = "mean"))) %>%
     mutate(sample = rep(1:sample_count)) %>%
-    pivot_longer(cols = starts_with("..."), names_to = "date")
+    pivot_longer(cols = starts_with("..."), names_to = "week")
  
   if(format == FALSE){
   return(death_forecast)
@@ -43,19 +61,17 @@ ts_deaths_only_forecast <- function(data,
   if(format == TRUE){
   # Get quantiles
   quantile <- death_forecast %>%
-    group_by(state, date) %>%
-    group_modify( ~ as.data.frame(quantile(.x$value, probs = quantiles_out))) %>%
+    group_by(state, week) %>%
+    group_modify( ~ as.data.frame(quantile(.x$value, probs = quantiles_out, na.rm = T))) %>%
     mutate(quantile = quantiles_out) %>%
     ungroup()
   
   # Format
-  dates_from <- unique(quantile$date)
-  forecast_weeks <- seq.Date(from = as.Date(max(data_weekly$week)+7), by = 7, length.out = horizon_weeks)
-  
+  dates_from <- unique(quantile$week)
   
   out <- quantile %>%
-    select(state, date, quantile, "deaths" = 3) %>%
-    mutate(date = recode(date, !!! setNames(forecast_weeks, dates_from)),
+    select(state, week, quantile, "deaths" = 3) %>%
+    mutate(week = recode(week, !!! setNames(forecast_weeks, dates_from)),
            deaths = ifelse(deaths < 0, 0, deaths),
            deaths = round(deaths),
            model_type = "deaths_only",
