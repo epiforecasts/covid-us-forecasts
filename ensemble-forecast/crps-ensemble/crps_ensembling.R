8df989dd48ada348c8bf09428c89be09ac535500
nikosbosse <nikosbosse@gmail.com>
2020-06-26 13:50:40 +0200

    changed folder name to ensemble-forecasts and added functionality for qra

---
+++ ensemble-forecast/crps-ensemble/crps_ensembling.R
@@ -0,0 +1,142 @@
+# # ============================================================================ #
+# # do ensembling with predictive samples
+# # ============================================================================ #
+# 
+# 
+# # latest rt estimates to csv
+# # --------------------------------
+# 
+# # load all rt forecasts into one data.table
+# rt_forecast <- load_all_rt_forecasts(forecast_dates = "latest", 
+#                                      forecast_adjustment = 16, 
+#                                      weekly = FALSE)
+# 
+# # save rt-forecasts
+# data.table::fwrite(rt_forecast, here::here("ensembling", "latest_forecasts", "latest_rt_forecast.csv"))
+# 
+# toy <- rt_forecast %>%
+#   dplyr::mutate(deaths = deaths + 1, 
+#                 model = "toymodel")
+# 
+# data.table::fwrite(toy, here::here("ensembling", "latest_forecasts", "latest_toy_forecast.csv"))
+# 
+# # latest timeseries estimates to csv
+# # --------------------------------
+# 
+# # missing
+# 
+# # code can be either here - then we can also drop the csv files completely
+# # or in the timeseries folder and here we just load in csv files
+# 
+# 
+# 
+# 
+# # load all predictions from csv
+# # --------------------------------
+# 
+# files <- list.files(here::here("ensembling", "latest_forecasts"))
+# 
+# 
+# # combine data and rename columns
+# df <- purrr::map_dfr(files, 
+#                      function(file) {
+#                        data.table::fread(here::here("ensembling", "latest_forecasts", file))
+#                      }) %>%
+#   dplyr::rename(y_pred = deaths, 
+#                 geography = region,
+#                 sample_nr = sample) %>%
+#   # data issue
+#   dplyr::filter(geography %in% c("US", "Alabama"))
+# 
+# ## some data issue: not all regions have the full number of samples
+# # filter_out <- df %>% 
+# #   dplyr::group_by(geography) %>%
+# #   dplyr::summarise(n = dplyr::n())
+# 
+# # read in weights
+# weights <- data.table::fread(here::here("ensembling", "stacking_weights.csv"))
+# w <- weights$stacking_weights
+# # w <- rep(1/2, length(w))
+# 
+# mix <- stackr::mixture_from_samples(df, w)
+# 
+# # write ensemble to csv for submission
+# 
+# 
+# ## Test Zone
+# 
+# 
+# forecast_date <- as.Date(unique(df$forecast_date))
+# 
+# int_forecast_date <- dplyr::case_when(weekdays(forecast_date) == "Sunday" ~ forecast_date,
+#                                       TRUE ~ as.Date(lubridate::floor_date(forecast_date, unit = "week", week_start = 7)))
+# 
+# state_codes <- tigris::fips_codes %>%
+#   dplyr::select(state_code, state_name) %>%
+#   unique() %>%
+#   rbind(c("US", "US"))
+# 
+# 
+# inc <- mix %>% 
+#   dplyr::rename(sample = sample_nr, 
+#                 cases = y_pred) %>%
+#   dplyr::mutate(sample = as.integer(sample), 
+#                 date = as.Date(date)) %>%
+#   # # filter by samples temporary fix until this is addressed in EpiNow/estimate_R0.R
+#   # dplyr::group_by(sample, date) %>%
+#   # dplyr::slice(1) %>%
+#   # dplyr::ungroup() %>%
+#   dplyr::mutate(week = lubridate::floor_date(date, unit = "week", week_start = 7)) %>% 
+#   dplyr::filter(date >= int_forecast_date) %>%
+#   dplyr::group_by(sample, week) %>%
+#   dplyr::summarise(week_cases = sum(cases, na.rm = TRUE)) %>%
+#   dplyr::ungroup()
+# 
+# cum <- inc %>%
+#   dplyr::group_by(sample) %>%
+#   dplyr::mutate(week_cases = cumsum(week_cases) + cumulative_deaths_data)
+# 
+# 
+# # =============================================================================
+# # take the function from format_submission.R and take it outside the function
+# # to reuse here. 
+# process_data = function(df, name){
+#   
+#   df <- df %>%
+#     dplyr::group_by(week) %>%
+#     dplyr::group_modify( ~ {
+#       quantile(.x$week_cases, probs = c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99)) %>%
+#         tibble::enframe(name = "quantile", value = "value") %>%
+#         dplyr::mutate(quantile = as.numeric(stringr::str_remove(quantile, "%"))/100)
+#     }) %>%
+#     dplyr::mutate(forecast_date = forecast_date,
+#                   target_end_date = week + 6,
+#                   location = state_codes$state_code[state_codes$state_name == loc_name],
+#                   type = "quantile",
+#                   horizon = ceiling(as.numeric(difftime(target_end_date, forecast_date, unit = "week"))),
+#                   target = paste(ceiling(as.numeric(difftime(target_end_date, forecast_date, unit = "week"))),
+#                                  "wk ahead",
+#                                  name,
+#                                  "death",
+#                                  sep = " ")) %>%
+#     dplyr::ungroup() %>%
+#     dplyr::filter(horizon <= 4) %>%
+#     dplyr::select(forecast_date, target, target_end_date, location, type, quantile, value)
+#   
+#   df <- df %>%
+#     dplyr::bind_rows(df %>%
+#                        dplyr::filter(quantile == 0.5) %>%
+#                        dplyr::mutate(type = "point") %>%
+#                        dplyr::select(-quantile)) %>%
+#     dplyr::mutate(value = floor(value))
+#   
+#   return(df)
+#   
+# }
+# # =============================================================================
+# 
+# out <- process_data(inc, "inc") %>%
+#   dplyr::bind_rows(process_data(cum, "cum"))
+
+
+
---
+++ ensemble-forecast/qra-ensemble/quantile_ensembling.R
@@ -0,0 +1,209 @@
+# ============================================================================ #
+# do ensembling with quantiles
+# ============================================================================ #
+
+library(magrittr)
+
+# load deaths data -------------------------------------------------------------
+death_data_inc <- load_observed_deaths(weekly = TRUE) %>%
+  dplyr::rename(geography = region, 
+                value = deaths) %>%
+  dplyr::mutate(value_type = "inc", 
+                geography = ifelse(geography == "US", "country", geography), 
+                # change date to end of week 
+                value_date = week + 6) %>%
+  dplyr::select(-week, -period, -data_type)
+
+death_data_cum <- load_observed_deaths(weekly = TRUE, cumulative = TRUE) %>%
+  dplyr::rename(geography = region, 
+                value = deaths) %>%
+  dplyr::mutate(value_type = "cum", 
+                geography = ifelse(geography == "US", "country", geography), 
+                # change date to end of week 
+                value_date = week + 6) %>%
+  dplyr::select(-week, -period, -data_type)
+
+
+death_data <- data.table::rbindlist(list(death_data_inc, death_data_cum))
+
+
+# load previous forecasts ------------------------------------------------------
+# rt files
+rt_files <- list.files(here::here("rt-forecast", "submission-files"))
+rt_paths <- paste("rt-forecast/submission-files/", rt_files, sep = "")
+
+rt_forecasts <- purrr::map_dfr(rt_paths, 
+                               .f = data.table::fread) %>%
+  dplyr::mutate(model = "Rt forecast")
+
+# timeseries forecasts
+deaths_only_files <- list.files(here::here("timeseries-forecast", "deaths-only", "submission-files"))
+deaths_only_paths <- paste("timeseries-forecast/deaths-only/submission-files/", 
+                           deaths_only_files, sep = "")
+
+deaths_only_forecasts <- purrr::map_dfr(deaths_only_paths, 
+                                        .f = data.table::fread) %>%
+  dplyr::mutate(model = "Deaths only forecast")
+
+
+# timeseries forecasts
+deaths_on_cases_files <- list.files(here::here("timeseries-forecast", "deaths-on-cases", "submission-files"))
+deaths_on_cases_paths <- paste("timeseries-forecast/deaths-on-cases/submission-files/", 
+                               deaths_on_cases_files, sep = "")
+
+
+deaths_on_cases_forecasts <- purrr::map_dfr(deaths_on_cases_paths, 
+                                            .f = data.table::fread) %>%
+  dplyr::mutate(model = "Deaths on cases forecast")
+
+# bind together 
+forecasts <- data.table::rbindlist(list(rt_forecasts, deaths_only_forecasts, 
+                                        deaths_on_cases_forecasts))
+
+# format forecasts -------------------------------------------------------------
+
+# load US location codes
+state_codes <- tigris::fips_codes %>%
+  dplyr::select(state_code, state_name) %>%
+  unique() %>%
+  rbind(c("US", "US")) %>%
+  dplyr::rename(location = state_code)
+
+# join forecasts with state codes
+# format to get the appropriate format for qra
+
+forecasts <- forecasts %>%
+  dplyr::inner_join(state_codes) %>%
+  dplyr::rename(creation_date = forecast_date, 
+                value_date = target_end_date,
+                geography = state_name) %>%
+  dplyr::mutate(geography_scale = ifelse(geography == "US", "country", "region"), 
+                geography = ifelse(geography == "US", "country", geography), 
+                inc = ifelse(grepl("inc", target), "inc", "cum"), 
+                value_date = as.Date(value_date), 
+                creation_date = as.Date(creation_date), 
+                value_type = inc) %>%
+  dplyr::filter(type == "quantile") %>%
+  dplyr::select(-target, -type, -inc)
+
+
+# do quantile regression and format output -------------------------------------
+
+res <- qra::qra(forecasts, death_data, pool = c("horizon", "geography"),
+                min_date = max(forecasts$creation_date) - 13)
+
+# format results
+# weekly incidences
+qra_forecast <- res$ensemble %>%
+  dplyr::rename(forecast_date = creation_date, 
+                target_end_date = value_date) %>%
+  dplyr::mutate(target = paste(ceiling(as.numeric(difftime(target_end_date, forecast_date, unit = "week"))),
+                               "wk ahead",
+                               value_type,
+                               "death",
+                               sep = " "), 
+                type = "quantile") %>%
+  dplyr::select(forecast_date, target, target_end_date, location, type, quantile, value)
+
+
+data.table::fwrite(qra_forecast, here::here("ensemble-forecast", "qra-ensemble", "submission-files",
+                                    paste0(forecast_date, "-epiforecasts-ensemble1.csv")))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# old code
+
+
+
+# 
+# 
+# 
+# # read in csvs ----------------------------------------------------------------- 
+# # list all submissions and take the most recent one
+# # alternatively switch to using a date in the future?
+# paths <- list()
+# 
+# 
+# ## get path of rt-submission
+# rt_files <- list.files(here::here("rt-forecast", "submission-files"))
+# rt_file <- sort(rt_files, decreasing = TRUE)[1]
+# paths[["rt-forecast"]] <- here::here("rt-forecast", "submission-files", rt_file)
+# 
+# ## get path of timeseries-submissions
+# # deaths-on-cases
+# paths[["timeseries-deaths-on-cases"]] <- here::here("timeseries-forecast", 
+#                                                     "deaths-on-cases",
+#                                                     "submission-files",
+#                                                     "latest-weekly-deaths-on-cases.csv")
+# 
+# # deaths only
+# paths[["timeseries-deaths-only"]] <- here::here("timeseries-forecast",
+#                                                 "deaths-only",
+#                                                 "submission-files",
+#                                                 "latest-weekly-deaths-only.csv")
+# 
+# # get forecast_date
+# forecast_date <- stringr::str_remove(rt_file, "-rt-forecast-submission.csv")
+# 
+# # load in all csvs
+# data <- purrr::map_dfr(paths, 
+#                        .f = data.table::fread, 
+#                        .id = "model")
+# 
+# 
+# # average quantiles ------------------------------------------------------------
+# 
+# # store models as strings
+# models <- data$model %>% 
+#   unique() 
+# n_models <- length(models)
+# 
+# # locations to submit --> take from rt forecast
+# locations <- data %>%
+#   dplyr::filter(model == "rt-forecast") %>%
+#   .$location %>%
+#   unique()
+# 
+# # pivot into wide format
+# data <- data %>%
+#   dplyr::mutate(quantile = round(quantile, digits = 2)) %>%
+#   tidyr::pivot_wider(names_from = model, 
+#                      values_from = value)
+# 
+# # get / set weights
+# w <- rep(1/n_models, n_models)
+# 
+# # take row means (replace with weighted mean in the futurue)
+# data <- as.data.table(data)[, ensemble := .(matrixStats::rowWeightedMeans(as.matrix(.SD), 
+#                                                                           na.rm = TRUE, 
+#                                                                           w = w)), 
+#                             .SDcols = models]
+# 
+# # deselect all old model columns and rename ensemble to value
+# data <- data %>%
+#   dplyr::select(!any_of(models)) %>%
+#   dplyr::rename(value = ensemble)
+# 
+# # filter out locations
+# data <- dplyr::filter(data, 
+#                       location %in% locations)
+# 
+# # store as csv submission ------------------------------------------------------
+# data.table::fwrite(data, here::here("final-submissions", "death-forecast", 
+#                                     paste0(forecast_date, "-epiforecasts-ensemble1.csv")))
+# 
