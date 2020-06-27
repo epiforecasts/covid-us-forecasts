91c8ba2b7fee738ae5e3b9e6688d1c41731a0f77
nikosbosse <nikosbosse@gmail.com>
2020-06-27 22:31:03 +0200

    edited evaluation,corrected data loading function

--- evaluation/evaluation.R
+++ evaluation/evaluation.R
@@ -1,64 +1,222 @@
-# ============================================================================ #
-# load all models vs. true values
-# ============================================================================ #
-
-files <- list.files(here::here("evaluation", "observed_vs_forecast"))
-
-# load in (toy) data
-files <- files[-1]
-
-# combine data
-df <- purrr::map_dfr(files, 
-                     function(file) {
-                       data.table::fread(here::here("evaluation", "observed_vs_forecast", file))
-                     })
-
+library(magrittr)
+library(data.table)
 
 # ============================================================================ #
-# evaluate forecasts
+# load all submissions
 # ============================================================================ #
 
-# do automatic scoring with scoringutils
-scored_forecasts <- df %>%
-  dplyr::group_by(horizon, region) %>%
-  tidyr::nest() %>%
-  dplyr::mutate(scores = purrr::map(data, 
-                                    function(x) {
-                                      scoringutils::eval_forecasts(x, summarised = TRUE)
-                                      })) %>%
-  tidyr::unnest(cols = c(data)) %>%
-  dplyr::group_by(forecast_date, id, region) %>%
-  dplyr::summarise(scores = unique(scores)) %>%
-  tidyr::unnest(cols = c(scores)) %>%
-  dplyr::mutate(horizon = as.numeric(as.Date(id) - as.Date(forecast_date)))
+# rt submissions ---------------------------------------------------------------
+rt_files <- list.files(here::here("rt-forecast", "submission-files"))
+rt_paths <- paste("rt-forecast/submission-files/", rt_files, sep = "")
 
-# also add median and iqr and ci
-# this is done to use pre-existing plotting functions
-missing_scores <- df %>%
-  dplyr::group_by(forecast_date, id, region, model) %>%
-  dplyr::summarise(median = median(predictions), 
-                   iqr = IQR(predictions), 
-                   ci = scoringutils::interval_score(true_values = predictions, 
-                                                     lower = quantile(predictions, prop = 0.025), 
-                                                     upper = quantile(predictions, prop = 0.975), 
-                                                     95))
+rt_forecasts <- purrr::map_dfr(rt_paths, 
+                               .f = data.table::fread) %>%
+  dplyr::mutate(model = "Rt forecast")
 
-# join together to obtain all scores and forecasts
-full <- dplyr::inner_join(scored_forecasts, missing_scores) %>%
-  dplyr::rename(dss = DSS, 
-                crps = CRPS, 
-                calibration = pit_p_val) %>%
-  dplyr::mutate(logs = NA)
+# deaths-only forecasts --------------------------------------------------------
+deaths_only_files <- list.files(here::here("timeseries-forecast", "deaths-only", "submission-files"))
+deaths_only_paths <- paste("timeseries-forecast/deaths-only/submission-files/", 
+                           deaths_only_files, sep = "")
 
-# summarise scores
-summarised_scores <- EpiSoon::summarise_scores(full)
+deaths_only_forecasts <- purrr::map_dfr(deaths_only_paths, 
+                                        .f = data.table::fread) %>%
+  dplyr::mutate(model = "Deaths only forecast")
+
+
+# deaths-on-cases forecasts ----------------------------------------------------
+deaths_on_cases_files <- list.files(here::here("timeseries-forecast", "deaths-on-cases", "submission-files"))
+deaths_on_cases_paths <- paste("timeseries-forecast/deaths-on-cases/submission-files/", 
+                               deaths_on_cases_files, sep = "")
+
+deaths_on_cases_forecasts <- purrr::map_dfr(deaths_on_cases_paths, 
+                                            .f = data.table::fread) %>%
+  dplyr::mutate(model = "Deaths on cases forecast")
+
+
+# qra-ensemble forecasts ----------------------------------------------------
+qra_files <- list.files(here::here("ensemble-forecast", "qra-ensemble", "submission-files"))
+qra_paths <- paste("ensemble-forecast/qra-ensemble/submission-files/", 
+                   deaths_on_cases_files, sep = "")
+
+qra_forecasts <- purrr::map_dfr(deaths_on_cases_paths, 
+                                            .f = data.table::fread) %>%
+  dplyr::mutate(model = "qra ensemble")
+
+
+
+# bind together 
+forecasts <- data.table::rbindlist(list(rt_forecasts, deaths_only_forecasts, 
+                                        deaths_on_cases_forecasts, 
+                                        qra_forecasts)) %>%
+  dplyr::mutate(target_end_date = as.Date(target_end_date), 
+                quantile = round(quantile, digits = 3))
+
 
 
 
 # ============================================================================ #
-# plot evaluation
+# load death data
 # ============================================================================ #
 
-# use pre-existing functions
+# load incidence data ----------------------------------------------------------
+death_data_inc <- load_observed_deaths(weekly = TRUE) %>%
+  dplyr::rename(geography = region, 
+                obs_deaths =  deaths) %>%
+  dplyr::mutate(value_type = "inc", 
+                geography = ifelse(geography == "US", "country", geography), 
+                # change date to end of week 
+                target_end_date = week + 6) %>%
+  dplyr::select(-week, -period, -data_type)
+
+# load cumulative data ---------------------------------------------------------
+death_data_cum <- load_observed_deaths(weekly = TRUE, cumulative = TRUE) %>%
+  dplyr::rename(geography = region, 
+                obs_deaths =  deaths) %>%
+  dplyr::mutate(value_type = "cum", 
+                geography = ifelse(geography == "US", "country", geography), 
+                # change date to end of week 
+                target_end_date = week + 6) %>%
+  dplyr::select(-week, -period, -data_type)
+
+# bind together ----------------------------------------------------------------
+death_data <- data.table::rbindlist(list(death_data_inc, death_data_cum)) %>%
+  dplyr::rename(location = state_code) 
 
 
+# ============================================================================ #
+# combine deaths and forecasts and reformat
+# ============================================================================ #
+
+# bind together ----------------------------------------------------------------
+combined <- forecasts %>%
+  dplyr::inner_join(death_data)
+
+# format -----------------------------------------------------------------------
+# only score on incidences? --> discuss
+
+combined <- combined %>%
+  dplyr::filter(type == "quantile", 
+                value_type == "inc") %>%
+  dplyr::select(-value_type, -type) %>%
+  dplyr::mutate(range = abs(100 - 2 * 100 * as.numeric(quantile)), 
+                boundary = ifelse(quantile < 0.5, "lower", "upper"))
+
+# add lower bound for median a second time
+combined <- combined %>%
+  rbind(combined %>%
+          dplyr::filter(quantile == 0.5) %>%
+          dplyr::mutate(boundary = "lower"))
+
+
+
+# pivot wider
+combined <- combined %>%
+  tidyr::pivot_wider(values_from = value, names_from = c(boundary, range), 
+                     id_cols = c(forecast_date, target, target_end_date, location, model))
+
+
+data <- combined %>%
+  tidyr::pivot_wider(values_from = value, names_from = c(boundary), 
+                     id_cols = c(forecast_date, target, target_end_date, 
+                                 location, model, range, geography, obs_deaths)) %>%
+  as.data.table()
+
+
+
+data[, `:=`("Interval_Score", scoringutils::interval_score(obs_deaths, 
+                                                           lower, upper, range))]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# # ============================================================================ #
+# # load all models vs. true values
+# # ============================================================================ #
+# 
+# # files <- list.files(here::here("evaluation", "observed_vs_forecast"))
+# # 
+# # # load in (toy) data
+# # files <- files[-1]
+# # 
+# # # combine data
+# # df <- purrr::map_dfr(files, 
+# #                      function(file) {
+# #                        data.table::fread(here::here("evaluation", "observed_vs_forecast", file))
+# #                      })
+# 
+# 
+# # ============================================================================ #
+# # evaluate forecasts
+# # ============================================================================ #
+# 
+# # do automatic scoring with scoringutils
+# scored_forecasts <- df %>%
+#   dplyr::group_by(horizon, region) %>%
+#   tidyr::nest() %>%
+#   dplyr::mutate(scores = purrr::map(data, 
+#                                     function(x) {
+#                                       scoringutils::eval_forecasts(x, summarised = TRUE)
+#                                       })) %>%
+#   tidyr::unnest(cols = c(data)) %>%
+#   dplyr::group_by(forecast_date, id, region) %>%
+#   dplyr::summarise(scores = unique(scores)) %>%
+#   tidyr::unnest(cols = c(scores)) %>%
+#   dplyr::mutate(horizon = as.numeric(as.Date(id) - as.Date(forecast_date)))
+# 
+# # also add median and iqr and ci
+# # this is done to use pre-existing plotting functions
+# missing_scores <- df %>%
+#   dplyr::group_by(forecast_date, id, region, model) %>%
+#   dplyr::summarise(median = median(predictions), 
+#                    iqr = IQR(predictions), 
+#                    ci = scoringutils::interval_score(true_values = predictions, 
+#                                                      lower = quantile(predictions, prop = 0.025), 
+#                                                      upper = quantile(predictions, prop = 0.975), 
+#                                                      95))
+# 
+# # join together to obtain all scores and forecasts
+# full <- dplyr::inner_join(scored_forecasts, missing_scores) %>%
+#   dplyr::rename(dss = DSS, 
+#                 crps = CRPS, 
+#                 calibration = pit_p_val) %>%
+#   dplyr::mutate(logs = NA)
+# 
+# # summarise scores
+# summarised_scores <- EpiSoon::summarise_scores(full)
+# 
+# 
+# 
+# # ============================================================================ #
+# # plot evaluation
+# # ============================================================================ #
+# 
+# # use pre-existing functions
+# 
+# 
