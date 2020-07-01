58caac1c4f47cebf9b83da94a9931f14a7b17c56
sophiemeakin <sophiemeakin@outlook.com>
2020-07-01 16:31:27 +0100

    Small tweak to update-expert
    Former-commit-id: 6871d2bc8b9239f76837a5fb4fe4207cc15773f2
    Former-commit-id: c21f551814b79634ffbc5e64cef6d0568973067b

--- expert-forecast/update-expert.R
+++ expert-forecast/update-expert.R
@@ -4,17 +4,16 @@ source("expert-forecast/get-expert.R")
 source("expert-forecast/format-expert.R")
 source("expert-forecast/plot-expert.R")
 
------
 
 ## Define forecast submission date (Monday)
-f_date <- "2020-06-29"
+def_date <- "2020-06-22"
 
 ## Update expert elicitation data
 raw_data <- get_expert_elicitation()
 
 ## Format aggregated estimates for submission/ensembling
-agg_submission_data <- format_expert_elicitation(for_forecast_date = f_date)
+agg_submission_data <- format_expert_elicitation(for_forecast_date = def_date)
 
 ## Visualise results (with and without individual point estimates)
-plot_expert(for_forecast_date = f_date, individual = FALSE)
-plot_expert(for_forecast_date = f_date, individual = TRUE)
+plot_expert(for_forecast_date = def_date, individual = FALSE)
+plot_expert(for_forecast_date = def_date, individual = TRUE)
