375c9cf26d864f9eacc65786744ba8a72ed950e1
nikosbosse <nikosbosse@gmail.com>
2020-07-02 17:39:18 +0200

    moved and updated visualise-submissions function
    Former-commit-id: 7170d05d357cf2bb431331bfcd6d707fe4c47816
    Former-commit-id: 2886ff9c8335b9a6cf6566a2140ec300a8a937cd

---
+++ visualisation/visualise-submissions/update-visualise-submissions.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(RColorBrewer)
+
+
+source(here::here("visualisation", "visualise-submissions", 
+                  "visualise-submission-functions.R"))
+
+
+submission_date <- Sys.Date()
+
+if(!dir.exists(here::here("visualisation", "visualise-submissions", 
+                          "submission-plots", 
+                          submission_date))) {
+  dir.create(here::here("visualisation", "visualise-submissions", 
+                        "submission-plots", 
+                        submission_date))
+}
+
+
+national_plot <- plot_forecasts()
+
+
+ggsave(here::here("visualisation", "visualise-submissions", "submission-plots", 
+                  submission_date, "submission_national.png"), 
+       plot = national_plot, 
+       width = 10, height = 10)
+
+
+
+subnational_plot <- plot_forecasts(national = FALSE)
+
+
+ggsave(here::here("visualisation", "visualise-submissions", "submission-plots", 
+                  submission_date, "submission_subnational.png"), 
+       plot = subnational_plot, 
+       width = 20, height = 20)
