febd9b47c4368ebde33ddfe13b2faeb62336802a
kathsherratt <katharine.sherratt@lshtm.ac.uk>
2020-07-03 20:21:35 +0100

    Enable end-to-end run
    - update-forecasts.R enables a complete end to end forecast update
    - this ultimately produces a mean ensemble in submission ready csv, which includes the US and states w/ over 100 cases in the last week
    - removed run_everything.R
    - split timeseries and Rt formatting functions so that function and call to function are in separate scripts
    - moved some unused files to "utils/out-of-date"
    
    
    Former-commit-id: 91d972389dccb49fdeadad530c5b5f802c34d329
    Former-commit-id: e7557811241ae67819ef52295dc250cc84d5984c

--- timeseries-forecast/format-timeseries-fn.R
+++ timeseries-forecast/format-timeseries-fn.R
@@ -127,37 +127,3 @@ return(out)
 
 
 
-# Run function for each model type ----------------------------------------
-
-# Deaths only
-
-model_type <- "deaths-only"
-deaths_only <- format_timeseries(right_truncate_weeks = 1, model_type = model_type)
-forecast_date <- unique(deaths_only$forecast_date)
-# Save under "latest"
-readr::write_csv(deaths_only, here::here("timeseries-forecast", model_type, 
-                                         "submission-files",
-                                         paste0("latest-weekly-", model_type, ".csv")))
-# Save under forecast date
-readr::write_csv(deaths_only, here::here("timeseries-forecast", model_type, "submission-files", 
-                                         paste0(forecast_date, "-", model_type, ".csv")))
-                
-
-# Deaths on cases
-
-model_type <- "deaths-on-cases"
-deaths_on_cases <- format_timeseries(right_truncate_weeks = 1, model_type = model_type)
-forecast_date <- unique(deaths_on_cases$forecast_date)
-
-# Save under "latest"
-readr::write_csv(deaths_on_cases, here::here("timeseries-forecast", model_type, 
-                                             "submission-files",
-                                             paste0("latest-weekly-", model_type, ".csv")))
-# Save under forecast date
-readr::write_csv(deaths_on_cases, here::here("timeseries-forecast", model_type, "submission-files", 
-                                             paste0(forecast_date, "-", model_type, ".csv")))
-
-
-
-
-
