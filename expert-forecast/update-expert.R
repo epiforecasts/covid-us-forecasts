adec5b08809a881a0788c92567c0a5a7115a81ee
sophiemeakin <sophiemeakin@outlook.com>
2020-07-03 10:24:29 +0100

    Update get-expert and update-expert
    * Update get-expert with new sheet ID
    * Formatting edits to update-expert

--- expert-forecast/update-expert.R
+++ expert-forecast/update-expert.R
@@ -1,19 +1,25 @@
+# Download and format expert elicitation forecasts for given forecast date
 library(tidyverse); library(googlesheets4); library(SHELF); library(RColorBrewer)
 
+# Set up functions
 source("expert-forecast/get-expert.R")
 source("expert-forecast/format-expert.R")
 source("expert-forecast/plot-expert.R")
 
 
-## Define forecast submission date (Monday)
+# Define forecast submission date (a Monday)
 def_date <- "2020-06-29"
 
-## Update expert elicitation data
-raw_data <- get_expert_elicitation()
 
-## Format aggregated estimates for submission/ensembling
-agg_submission_data <- format_expert_elicitation(for_forecast_date = def_date)
+# Download most recent expert elicitation forecasts
+get_expert_elicitation()
 
-## Visualise results (with and without individual point estimates)
-plot_expert(for_forecast_date = def_date, individual = FALSE)
-plot_expert(for_forecast_date = def_date, individual = TRUE)
+
+# Format (mean aggregated) forecasts for submission/ensembling
+format_expert_elicitation(for_forecast_date = def_date,
+                          submission_quantiles = c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99))
+
+
+# Can visualise expert forecasts with plot_expert()
+# plot_expert(for_forecast_date = def_date, individual = FALSE)
+# plot_expert(for_forecast_date = def_date, individual = TRUE)
