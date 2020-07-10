547bd40997bef106b7992e8c6f11a2956d952a5e
nikosbosse <nikosbosse@gmail.com>
2020-07-10 14:56:34 +0200

    simplified mean ensembling script
    Former-commit-id: 4fe2ad57bf23e851c028ebaf0ad3c523bcc4394b
    Former-commit-id: 95c75935b05ce939f46462d24101cea148a5ad34

--- ensembling/quantile-average/quantile-average-equal-weights.R
+++ ensembling/quantile-average/quantile-average-equal-weights.R
@@ -1,46 +1,13 @@
 
-# Load required packages --------------------------------------------------
+# Load required packages and functions -----------------------------------------
 
 library(magrittr)
-library(purrr)
-library(here)
-library(data.table)
-library(dplyr)
-
-# average quantiles
-# for qra go to quantile-ensembling.R
-
-# read in csvs -----------------------------------------------------------------
-# list all submissions and take the most recent one
-# alternatively switch to using a date in the future?
-paths <- list()
+source(here::here("utils", "load-submissions-function.R"))
 
 
-## get path of rt-submission
-paths[["rt-forecast"]] <- here::here("rt-forecast",
-                                      "submission-files",
-                                      "latest-rt-forecast-submission.csv")
-
-## get path of timeseries-submissions
-# deaths-on-cases
-paths[["timeseries-deaths-on-cases"]] <- here::here("timeseries-forecast",
-                                                    "deaths-on-cases",
-                                                    "submission-files",
-                                                    "latest-weekly-deaths-on-cases.csv")
-
-# deaths only
-paths[["timeseries-deaths-only"]] <- here::here("timeseries-forecast",
-                                                "deaths-only",
-                                                "submission-files",
-                                                "latest-weekly-deaths-only.csv")
-
-# expert elicitation
-
-
-# load in all csvs
-data <- purrr::map_dfr(paths,
-                       .f = data.table::fread,
-                       .id = "model")
+# Load Forecasts ---------------------------------------------------------------
+forecasts <- load_submission_files(dates = "latest",
+                                   models = c("rt", "deaths-only", "deaths-on-cases"))
 
 # get forecast_date
 forecast_date <- unique(dplyr::pull(data, forecast_date))
@@ -49,42 +16,33 @@ forecast_date <- unique(dplyr::pull(data, forecast_date))
 # average quantiles ------------------------------------------------------------
 
 # store models as strings
-models <- data$model %>%
-  unique()
-n_models <- length(models)
+models <- unique(forecasts$model)
 
 
-# pivot into wide format
-data <- data %>%
+# pivot_wider
+forecasts_wide <- forecasts %>%
   dplyr::mutate(quantile = round(quantile, digits = 3)) %>%
   tidyr::pivot_wider(names_from = model,
                      values_from = value)
 
 # add average; rename ensemble to value
-data <- data %>%
- dplyr::mutate(ensemble = data %>% 
-                  dplyr::select(all_of(models)) %>%
-                  rowMeans(na.rm = TRUE)) %>%
-  dplyr::rename(value = ensemble)
-
-# # filter out locations
-# # locations to submit = 100+ deaths last week
-# source(here::here("utils", "states-min-last-week.R"))
-# keep_states <- states_min_last_week(min_last_week = 100, last_week = 1)
-# 
-# data <- dplyr::filter(data,
-#                       location %in% keep_states$state_code)
-
-# Deselect model specific columns
-data <- dplyr::select(data, -dplyr::any_of(models))
+mean_ensemble <- forecasts_wide %>%
+  dplyr::mutate(ensemble = forecasts_wide %>% 
+                  dplyr::select(dplyr::all_of(models)) %>%
+                  rowMeans()) %>%
+  dplyr::rename(value = ensemble) %>%
+  dplyr::select(-dplyr::all_of(models)) %>%
+  dplyr::select(forecast_date, target, target_end_date, location, type, quantile, value) %>%
+  # round values after ensembling
+  dplyr::mutate(value = round(value)) 
 
 
 # store as csv submission ------------------------------------------------------
 # Store in QA folder
 # Dated
-data.table::fwrite(data, here::here("ensembling", "quantile-average", 
-                                    "submission-files","dated",
-                                    paste0(forecast_date, "-epiforecasts-ensemble1-qa.csv")))
+data.table::fwrite(mean_ensemble, here::here("ensembling", "quantile-average", 
+                                             "submission-files","dated",
+                                             paste0(forecast_date, "-epiforecasts-ensemble1-qa.csv")))
 # Latest
-data.table::fwrite(data, here::here("ensembling", "quantile-average", "submission-files",
-                                    paste0("latest-epiforecasts-ensemble1-qa.csv")))
+data.table::fwrite(mean_ensemble, here::here("ensembling", "quantile-average", "submission-files",
+                                             paste0("latest-epiforecasts-ensemble1-qa.csv")))
