cf0d3ac967ee0e81eec340b5885f62666c85466e
kathsherratt <katharine.sherratt@lshtm.ac.uk>
2020-07-13 18:20:09 +0100

    Timeseries update and revised ensemble
    - deaths-on-cases forecast now includes leading case predictors
    - all ensembles updated with new timeseries forecast
    - final submission csv based on mean average
    
    
    Former-commit-id: 2af7a44abd8295a1fd24ff470ebf09bacbb07c66
    Former-commit-id: f1d1d7805a4decfcf2c93c689f90d4d7c8344ce8

--- timeseries-forecast/plot-timeseries-fn.R
+++ timeseries-forecast/plot-timeseries-fn.R
@@ -8,7 +8,7 @@
 
 library(ggplot2); library(dplyr); library(tidyr); library(stringr)
 
-plot_timeseries <- function(model_type, date = "latest-weekly", right_truncate_weeks = 1, 
+plot_timeseries <- function(model_type, date = "latest", right_truncate_weeks = 1, 
                             xlim_min = 10, id = NULL){
 
   # Read in forecast
@@ -23,6 +23,13 @@ plot_timeseries <- function(model_type, date = "latest-weekly", right_truncate_w
                          levels=c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")),
            epiweek = as.numeric(paste0(epiweek, ".", as.numeric(day))))
   
+  daily_cases_state <- get_us_cases(data = "daily") %>%
+    mutate(epiweek = lubridate::epiweek(date),
+           day = ordered(weekdays(as.Date(date)), 
+                         levels=c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")),
+           epiweek = as.numeric(paste0(epiweek, ".", as.numeric(day))))
+  
+  
   # States ------------------------------------------------------------------
   
   weekly_deaths_state <- daily_deaths_state %>%
@@ -31,11 +38,20 @@ plot_timeseries <- function(model_type, date = "latest-weekly", right_truncate_w
     group_by(state, epiweek) %>%
     summarise(deaths = sum(deaths))
   
+  weekly_cases_state <- daily_cases_state %>%
+    mutate(epiweek = lubridate::epiweek(date),
+           date = NULL) %>%
+    group_by(state, epiweek) %>%
+    summarise(cases = sum(cases))
+  
   keep_states <- filter(weekly_deaths_state, epiweek == max(epiweek)-1 & deaths > 50) %>%
     pull(state)
   
   weekly_deaths_state <- filter(weekly_deaths_state, state %in% keep_states)
   daily_deaths_state <- filter(daily_deaths_state, state %in% keep_states)
+  
+  weekly_cases_state <- filter(weekly_cases_state, state %in% keep_states)
+  daily_cases_state <- filter(daily_cases_state, state %in% keep_states)
   
   # Reshape forecast for plotting
   fc_state <- weekly_forecast %>%
@@ -53,6 +69,7 @@ plot_timeseries <- function(model_type, date = "latest-weekly", right_truncate_w
     ggplot2::geom_line(ggplot2::aes(y = c0.5)) +
     ggplot2::geom_line(data = weekly_deaths_state, ggplot2::aes(x = epiweek, y = deaths), col = "blue") +
     ggplot2::geom_line(data = daily_deaths_state, ggplot2::aes(x = epiweek, y = deaths), col = "light blue") +
+    ggplot2::geom_line(data = weekly_cases_state, ggplot2::aes(x = epiweek, y = cases/100), col = "grey") +
     ggplot2::geom_ribbon(alpha = 0.2, ggplot2::aes(ymin = c0.05, ymax = c0.95)) +
     ggplot2::geom_ribbon(alpha = 0.2, ggplot2::aes(ymin = c0.25, ymax = c0.75)) +
     ggplot2::facet_wrap("state", scales = "free_y") +
@@ -61,7 +78,8 @@ plot_timeseries <- function(model_type, date = "latest-weekly", right_truncate_w
     ggplot2::ylab("Incident deaths") +
     ggplot2::xlab("Epiweek") +
     ggplot2::geom_vline(xintercept = max(weekly_deaths_state$epiweek, na.rm=T) - right_truncate_weeks, lty = 2) +
-    ggplot2::labs(caption = "--- is date of data truncation",
+    ggplot2::labs(caption = "--- is date of data truncation
+                  Cases are divided by 100 for scale",
                   title = paste0("Incident deaths in US states, from ", model_type, " model"))
   
   
@@ -82,6 +100,17 @@ plot_timeseries <- function(model_type, date = "latest-weekly", right_truncate_w
     group_by(epiweek) %>%
     summarise(deaths = sum(deaths))
   
+  daily_cases_national <- daily_cases_state %>%
+    mutate(state = "US") %>%
+    group_by(epiweek) %>%
+    summarise(cases = sum(cases))
+  
+  weekly_cases_national <- daily_cases_state %>%
+    mutate(state = "US",
+           epiweek = lubridate::epiweek(date)) %>%
+    group_by(epiweek) %>%
+    summarise(cases = sum(cases))
+  
   
   # Reshape forecast for plotting
   fc_national <- weekly_forecast %>%
@@ -98,6 +127,7 @@ plot_timeseries <- function(model_type, date = "latest-weekly", right_truncate_w
     ggplot2::geom_line(ggplot2::aes(y = c0.5)) +
     ggplot2::geom_line(data = weekly_deaths_national, ggplot2::aes(x = epiweek, y = deaths), col = "blue") +
     ggplot2::geom_line(data = daily_deaths_national, ggplot2::aes(x = epiweek, y = deaths), col = "light blue") +
+    ggplot2::geom_line(data = weekly_cases_national, ggplot2::aes(x = epiweek, y = cases/100), col = "grey") +
     ggplot2::geom_ribbon(alpha = 0.2, ggplot2::aes(ymin = c0.05, ymax = c0.95)) +
     ggplot2::geom_ribbon(alpha = 0.2, ggplot2::aes(ymin = c0.25, ymax = c0.75)) +
     ggplot2::facet_wrap("state", scales = "free_y") +
@@ -106,7 +136,8 @@ plot_timeseries <- function(model_type, date = "latest-weekly", right_truncate_w
     ggplot2::ylab("Incident deaths") +
     ggplot2::xlab("Epiweek") +
     ggplot2::geom_vline(xintercept = max(weekly_deaths_national$epiweek) - right_truncate_weeks, lty = 2) +
-    ggplot2::labs(caption = "--- is date of data truncation",
+    ggplot2::labs(caption = "--- is date of data truncation
+                  Cases are divided by 100 for scale",
                   title = paste0("Incident deaths in US states, from ", model_type, " model"))
   
   ggplot2::ggsave(filename = paste0(id, date, "-national-", model_type, ".png"), plot = plot_national, 
