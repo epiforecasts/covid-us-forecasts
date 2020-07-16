0aa524603e35dd5fb3edb982e3d5372578666d98
kathsherratt <katharine.sherratt@lshtm.ac.uk>
2020-07-16 19:23:34 +0100

    Update instructions
    - README: instructions on how to update the weekly forecast
    - update-us-deaths-rt.R : removing some unused code for deps in; moving it to utils/package-check.R
    - update-final-submission.R : reset to mean average ensemble (this can be changed as needed)

--- utils/package-check.R
+++ utils/package-check.R
@@ -1,20 +1,30 @@
+# Checking all dependencies ----------------
+devtools::install_deps()
+
+library(EpiNow);library(EpiSoon)
 library(magrittr);library(furrr)
 library(dplyr);library(tidyr)
 library(readr);library(stringr)
 library(purrr);library(future)
 library(future);library(future.apply)
-library(here);library(sf); library(distill)
-library(knitr);library(kableExtra); library(forecastHybrid)
+library(here); library(forecastHybrid)
 library(quantgen);library(tigris); library(scoringutils)
 
-# Drat
-install.packages("drat")
-library(drat)
+# For Rt estimates -------------------------
+# Check that EpiNow / EpiSoon and their deps are up to date
+# install.packages("drat")
+# library(drat)
+# drat::addRepo("epiforecasts")
+# install.packages("EpiSoon")
+# library(devtools)
+# devtools::install_deps(repos = "https://epiforecasts.io/drat/")
 
+
+# For ensembling ----------------------------
 # Quantgen
-library(devtools)
-install.packages("Rglpk")
-install_github(repo="ryantibs/quantgen", subdir="R-package/quantgen")
+# library(devtools)
+# install.packages("Rglpk")
+# install_github(repo="ryantibs/quantgen", subdir="R-package/quantgen")
 
 # Up to date scoringutils
-remotes::install_github("epiforecasts/scoringutils")
+# remotes::install_github("epiforecasts/scoringutils")
