7494a1941f75c7ed99397ce10effe506ffd2a10b
kathsherratt <katharine.sherratt@lshtm.ac.uk>
2020-07-21 17:50:06 +0100

    revert
    revert commit to avoid conflicts with master

--- timeseries-forecast/historical-timeseries.R
+++ timeseries-forecast/historical-timeseries.R
@@ -11,7 +11,7 @@ deaths_state <- get_us_deaths(data = "daily")
 
 deaths_national <- deaths_state %>%
   group_by(date) %>%
-  summarise(deaths = sum(deaths), .groups = "drop_last") %>%
+  summarise(deaths = sum(deaths)) %>%
   mutate(state = "US")
 
 
@@ -19,19 +19,21 @@ cases_state <- get_us_cases(data = "daily")
 
 cases_national <- cases_state %>%
   group_by(date) %>%
-  summarise(cases = sum(cases), .groups = "drop_last") %>%
+  summarise(cases = sum(cases)) %>%
   mutate(state = "US")
 
-# Set historical paramaters
-submission_date <- "2020-06-15"
-weeks_into_past <- lubridate::epiweek(Sys.Date()) - lubridate::epiweek(submission_date)
+
+weeks_into_past <- 1
+right_truncation <- 1
 
 # Set forecast parameters -------------------------------------------------
-right_truncation <- 1 # weeks
 right_truncate_weeks <- weeks_into_past + right_truncation
 
 sample_count <- 1000
-horizon_weeks <- 4
+horizon_weeks <- 6
+
+format <- TRUE
+quantiles_out <- c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99)
 case_quantile <- 0.5
 
 
@@ -41,13 +43,17 @@ case_quantile <- 0.5
 state_deaths_only_forecast <- ts_deaths_only_forecast(data = deaths_state, 
                                                       sample_count = sample_count, 
                                                       horizon_weeks = horizon_weeks,
-                                                      right_truncate_weeks = right_truncate_weeks)
+                                                      right_truncate_weeks = right_truncate_weeks,
+                                                      format = TRUE,
+                                                      quantiles_out = quantiles_out)
 
 # National forecast
 national_deaths_only_forecast <- ts_deaths_only_forecast(data = deaths_national, 
                                                          sample_count = sample_count, 
                                                          horizon_weeks = horizon_weeks,
-                                                         right_truncate_weeks = right_truncate_weeks)
+                                                         right_truncate_weeks = right_truncate_weeks,
+                                                         format = TRUE,
+                                                         quantiles_out = quantiles_out)
 
 # Bind and save daily forecast
 deaths_only_forecast <- bind_rows(national_deaths_only_forecast, state_deaths_only_forecast) %>%
@@ -55,7 +61,7 @@ deaths_only_forecast <- bind_rows(national_deaths_only_forecast, state_deaths_on
 
 date_created <- pull(deaths_only_forecast, date_created)[1]
 
-saveRDS(deaths_only_forecast, here::here("timeseries-forecast", "deaths-only", "raw-samples",
+saveRDS(deaths_only_forecast, here::here("timeseries-forecast", "deaths-only", "raw-rds",
                                          paste0(date_created, "-deaths-only.rds")))
 
 
@@ -94,36 +100,156 @@ saveRDS(deaths_only_forecast, here::here("timeseries-forecast", "deaths-on-cases
 
 
 
-# # Format ------------------------------------------------------------------
-# source(here::here("timeseries-forecast", "format-timeseries-fn.R"))
-# quantiles_out <- c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99)
-# 
-# 
-# # Run function for each model type ----------------------------------------
-# 
-# # Deaths only
-# 
-# model_type <- "deaths-only"
-# deaths_only <- format_timeseries(right_truncate_weeks = right_truncate_weeks, 
-#                                  model_type = model_type,
-#                                  quantiles_out = quantiles_out)
-# forecast_date <- unique(deaths_only$forecast_date)
-# 
-# # Save under forecast date
-# readr::write_csv(deaths_only, here::here("timeseries-forecast", model_type, 
-#                                          "submission-files", "dated",
-#                                          paste0(forecast_date, "-", model_type, ".csv")))
-# 
-# 
-# # Deaths on cases
-# 
-# model_type <- "deaths-on-cases"
-# deaths_on_cases <- format_timeseries(right_truncate_weeks = right_truncate_weeks, 
-#                                      model_type = model_type,
-#                                      quantiles_out = quantiles_out)
-# forecast_date <- unique(deaths_on_cases$forecast_date)
-# 
-# # Save under forecast date
-# readr::write_csv(deaths_on_cases, here::here("timeseries-forecast", model_type, 
-#                                              "submission-files", "dated",
-#                                              paste0(forecast_date, "-", model_type, ".csv")))
+# Format ------------------------------------------------------------------
+
+format_timeseries <- function(model_type, right_truncate_weeks, date_created){
+  
+# Raw data set up -------------------------------------------------------------
+
+# Get state codes
+state_codes <- tigris::fips_codes %>%
+  dplyr::select(state_code, state_name) %>%
+  unique() %>%
+  rbind(c("US", "US"))
+
+# Read in forecast
+raw_weekly_forecast <- readRDS(here::here("timeseries-forecast", model_type, "raw-rds",
+                                          paste0(date_created, "-", model_type, ".rds")))
+
+forecast_date <- unique(raw_weekly_forecast$date_created)
+
+# Set epiweek to target date conversion
+forecast_date_epiweek <- lubridate::epiweek(forecast_date)
+
+epiweek_to_date <- tibble::tibble(date = seq.Date(from = (forecast_date-1), by = 1, length.out = 42)) %>%
+  dplyr::mutate(epiweek = lubridate::epiweek(date),
+                day = weekdays(date)) %>%
+  dplyr::filter(day == "Saturday") %>%
+  dplyr::select(target_end_date = date, epiweek_target = epiweek)
+
+
+
+# Cumulative formatting ----------------------------------------------------
+
+# Get cumulative weekly data
+cumulative_data <- readRDS(here::here("data", "deaths-data-cumulative.rds")) %>%
+  dplyr::mutate(epiweek = lubridate::epiweek(date))
+
+# State cumulative data
+cumulative_deaths_state <- cumulative_data %>%
+  dplyr::group_by(epiweek) %>%
+  dplyr::filter(date == max(date)) %>%
+  dplyr::ungroup()
+
+# National cumulative data
+cumulative_deaths_national <- cumulative_data %>%
+  dplyr::group_by(epiweek) %>%
+  dplyr::filter(date == max(date)) %>%
+  dplyr::summarise(deaths = sum(deaths),
+                   state = "US") %>%
+  dplyr::ungroup()
+
+# Bind
+cumulative_deaths <- dplyr::bind_rows(cumulative_deaths_state, cumulative_deaths_national)
+
+# Filter to last week of data shown to model
+last_week_cumulative_deaths <- cumulative_deaths %>%
+  dplyr::filter(epiweek == max(epiweek)-right_truncate_weeks) %>%
+  dplyr::select(-epiweek, -date)
+
+# Create cumulative forecast
+cumulative_forecast <- raw_weekly_forecast %>%
+  dplyr::group_by(quantile, state) %>%
+  dplyr::mutate(deaths = cumsum(deaths)) %>%
+  ungroup()
+
+# Join historical cumulative deaths to forecasts  
+cumulative_forecast <- cumulative_forecast %>%  
+  dplyr::left_join(last_week_cumulative_deaths, by = "state") %>%
+  dplyr::mutate(deaths = deaths.x + deaths.y) %>%
+  # Filter to current and future epiweeks
+  dplyr::filter(epiweek_target >= forecast_date_epiweek) %>%
+  # Format for submission
+  dplyr::left_join(state_codes, by = c("state" = "state_name")) %>%
+  dplyr::left_join(epiweek_to_date, by = "epiweek_target") %>%
+  dplyr::mutate(forecast_date = date_created,
+                value = deaths,
+                type = "quantile",
+                target = paste(epiweek_target - forecast_date_epiweek + 1,
+                               "wk ahead cum death",
+                               sep = " "),
+                location = state_code) %>%
+  dplyr::select(forecast_date, target, target_end_date, location, type, quantile, value)
+
+# Add point forecast
+cumulative_forecast <- cumulative_forecast %>%
+  dplyr::bind_rows(cumulative_forecast %>%
+                     dplyr::filter(quantile == 0.5) %>%
+                     dplyr::mutate(type = "point") %>%
+                     dplyr::select(-quantile)) %>%
+  dplyr::mutate(value = floor(value))
+
+
+# Incident forecast -------------------------------------------
+
+incident_forecast <- raw_weekly_forecast %>%
+  # Filter to current and future epiweeks
+  dplyr::filter(epiweek_target >= forecast_date_epiweek) %>%
+  # Format for submission
+  dplyr::left_join(state_codes, by = c("state" = "state_name")) %>%
+  dplyr::left_join(epiweek_to_date, by = "epiweek_target") %>%
+  dplyr::mutate(forecast_date = date_created,
+                value = deaths,
+                type = "quantile",
+                target = paste(epiweek_target - forecast_date_epiweek + 1,
+                               "wk ahead inc death",
+                               sep = " "),
+                location = state_code) %>%
+  dplyr::select(forecast_date, target, target_end_date, location, type, quantile, value)
+
+# Add point forecast
+incident_forecast <- incident_forecast %>%
+  dplyr::bind_rows(incident_forecast %>%
+                     dplyr::filter(quantile == 0.5) %>%
+                     dplyr::mutate(type = "point") %>%
+                     dplyr::select(-quantile)) %>%
+  dplyr::mutate(value = floor(value))
+
+
+# Bind cumulative and incident --------------------------------------------
+
+out <- dplyr::bind_rows(incident_forecast, cumulative_forecast)
+
+return(out)
+}
+
+
+
+# Run function for each model type ----------------------------------------
+
+# Deaths only
+
+model_type <- "deaths-only"
+deaths_only <- format_timeseries(right_truncate_weeks = right_truncate_weeks, 
+                                 model_type = model_type,
+                                 date_created = date_created)
+forecast_date <- unique(deaths_only$forecast_date)
+
+# Save under forecast date
+readr::write_csv(deaths_only, here::here("timeseries-forecast", model_type, 
+                                         "submission-files", "dated",
+                                         paste0(forecast_date, "-", model_type, ".csv")))
+
+
+# Deaths on cases
+
+model_type <- "deaths-on-cases"
+deaths_on_cases <- format_timeseries(right_truncate_weeks = right_truncate_weeks, 
+                                     model_type = model_type,
+                                     date_created = date_created)
+forecast_date <- unique(deaths_on_cases$forecast_date)
+
+# Save under forecast date
+readr::write_csv(deaths_on_cases, here::here("timeseries-forecast", model_type, 
+                                             "submission-files", "dated",
+                                             paste0(forecast_date, "-", model_type, ".csv")))
