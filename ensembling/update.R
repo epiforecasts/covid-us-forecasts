63503c527ac4f1f07df34b760b1811025abdc41a
nikosbosse <nikosbosse@gmail.com>
2020-07-23 16:08:35 +0200

    updated past crps ensembles as good as possible
    Former-commit-id: 7f4f9012cce4278033d77f4cba3489d0d451ec26
    Former-commit-id: ddbce5db2a44b2c2424e0d84715e76b5e84716ff

--- ensembling/update.R
+++ ensembling/update.R
@@ -5,3 +5,7 @@ source(here::here("ensembling", "quantile-average",
 # run qra-ensemble
 source(here::here("ensembling", "qra-ensemble", 
                   "update-qra-ensemble.R"))
+
+# run crps-ensemble
+source(here::here("ensembling", "crps-ensemble", 
+                  "update-crps-ensemble.R"))
