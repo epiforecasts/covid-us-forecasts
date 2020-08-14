aa114ec41bee264c71dcd32f560100eb65f0538e
kathsherratt <katharine.sherratt@lshtm.ac.uk>
2020-08-14 18:32:26 +0100

    Infinite Rt models
    Add as many Rt model variants as needed.
    General instructions to add a new Rt model:
    -  `update-rt-forecast.R` - add a name to the list of models on line 71
    -  `update-rt-forecast.R` - add the Epinow2 code to the bottom; set target_dir and summary_dir to targets[[model_name]] and summary[[model_name]
    - Add the same model name to the top of `update-rt-format.R`
    - Add to the model list `meta-model-list.R` - but this bit is still in dev
    This PR sets up "original" and "fixed_rt" models (both on same settings for @seabbs to adjust)

--- rt-forecast-2/format/update-rt-format.R
+++ rt-forecast-2/format/update-rt-format.R
@@ -9,61 +9,67 @@ require(ggplot2)
 require(cowplot)
 require(data.table)
 
-# Control parameters ------------------------------------------------------
+rt_file_names <- c("original", "fixed_rt")
 
-forecast_dir <- here::here("rt-forecast-2/forecast/deaths") 
+for(i in rt_models){
 
-# Load forecasts ----------------------------------------------------------
+  # Set directory ------------------------------------------------------
 
-# Latest forecast
+  forecast_dir <- paste0(here::here("rt-forecast-2/forecast/deaths_forecast", i, "/"))
+  output_dir <- paste0(here::here("rt-forecast-2/output", i, "/"))
+  
+  
+  # Load forecasts ----------------------------------------------------------
+  
+  # Latest forecast
+  
+  source(here::here("utils", "current-forecast-submission-date.R"))
+  
+  forecasts_raw <- EpiNow2::get_regional_results(results_dir = paste0(forecast_dir, "state"),
+                                                 date = "latest", forecast = TRUE)$estimated_reported_cases$samples
+  
+  
+  # Format past forecasts ---------------------------------------------------
+  # 
+  # forecast_date <- "2020-07-19"
+  # submission_date <- "2020-07-20"
+  # 
+  # forecasts_raw <- EpiNow2::get_regional_results(results_dir = here::here("rt-forecast-2/forecast/deaths/state"),
+  #                                                date = forecast_date, forecast = TRUE)
+  # forecasts_raw <- forecasts_raw$estimated_reported_cases$samples
+  
+  # Format samples ----------------------------------------------------------
+  data.table::setnames(forecasts_raw, old = c("region", "cases"), new = c("state", "deaths"))
+  
+  forecasts_samples <- forecasts_raw[, .(sample = sample,
+                                         deaths = deaths,
+                                         target_end_date = date,
+                                         forecast_date = forecast_date,
+                                         model = "Rt2-original",
+                                         location = state)]
+  
+  forecasts_samples <- forecasts_samples[target_end_date > forecast_date]
+  
+  # Save samples
+  saveRDS(forecasts_samples, 
+          paste0(output_dir, "samples/", forecast_date, "-rt-forecast-samples.rds"))
+  
+  
+  # Format forecasts --------------------------------------------------------
+  source(here::here("rt-forecast-2/format/utils/format_forecast_us.R"))
+  
+  formatted_forecasts <- format_forecast_us(forecasts = forecasts_raw, 
+                                            forecast_date = forecast_date, 
+                                            submission_date = submission_date)
+  
+  
+  # Save forecast -----------------------------------------------------------
+  
+  readr::write_csv(formatted_forecasts, 
+                   paste0(output_dir, "submission-files/dated/", forecast_date, "-rt-2-forecast.csv"))
+  
+  readr::write_csv(formatted_forecasts, 
+                   paste0(output_dir, "submission-files/latest-rt-2-forecast.csv"))
 
-source(here::here("utils", "current-forecast-submission-date.R"))
-
-
-forecasts_raw <- EpiNow2::get_regional_results(results_dir = here::here("rt-forecast-2/forecast/deaths/state"),
-                                              date = "latest", forecast = TRUE)$estimated_reported_cases$samples
-
-
-# Format past forecasts ---------------------------------------------------
-# 
-# forecast_date <- "2020-07-19"
-# submission_date <- "2020-07-20"
-# 
-# forecasts_raw <- EpiNow2::get_regional_results(results_dir = here::here("rt-forecast-2/forecast/deaths/state"),
-#                                                date = forecast_date, forecast = TRUE)
-# forecasts_raw <- forecasts_raw$estimated_reported_cases$samples
-
-# Format samples ----------------------------------------------------------
-data.table::setnames(forecasts_raw, old = c("region", "cases"), new = c("state", "deaths"))
-
-forecasts_samples <- forecasts_raw[, .(sample = sample,
-                                       deaths = deaths,
-                                       target_end_date = date,
-                                       forecast_date = forecast_date,
-                                       model = "Rt-Epinow2",
-                                       location = state)]
-
-forecasts_samples <- forecasts_samples[target_end_date > forecast_date]
-
-# Save samples
-saveRDS(forecasts_samples, 
-        paste0("rt-forecast-2/output/samples/", forecast_date, "-rt-forecast-samples.rds"))
-
-
-# Format forecasts --------------------------------------------------------
-source(here::here("rt-forecast-2/format/utils/format_forecast_us.R"))
-
-formatted_forecasts <- format_forecast_us(forecasts = forecasts_raw, 
-                                          forecast_date = forecast_date, 
-                                          submission_date = submission_date)
-
-
-# Save forecast -----------------------------------------------------------
-
-readr::write_csv(formatted_forecasts, 
-                 paste0("rt-forecast-2/output/submission-files/dated/", forecast_date, "-rt-2-forecast.csv"))
-
-readr::write_csv(formatted_forecasts, 
-                 paste0("rt-forecast-2/output/submission-files/latest-rt-2-forecast.csv"))
-
+}
 
