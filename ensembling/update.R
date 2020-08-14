8e777be2639e530247555b36b1173e40ea38a390
kathsherratt <katharine.sherratt@lshtm.ac.uk>
2020-08-14 15:40:21 +0100

    Update update.R
    Former-commit-id: 386c0ebcb83d726779be8295b0e0f0656564c14b
    Former-commit-id: 1ca168c96f928b8cedc9e7d9f6525ed6fbf4e288

--- ensembling/update.R
+++ ensembling/update.R
@@ -2,10 +2,14 @@
 source(here::here("ensembling", "quantile-average", 
                   "update-equal-quantile-average.R"))
 
-# run qra-ensemble
+# run qra ensemble
 source(here::here("ensembling", "qra-ensemble", 
                   "update-qra-ensemble.R"))
 
-# run crps-ensemble
+# run qra-by-state ensemble
+source(here::here("ensembling", "qra-state-ensemble", 
+                  "update-state-qra-ensemble.R"))
+
+# run crps ensemble
 source(here::here("ensembling", "crps-ensemble",
                   "update-crps-ensemble.R"))
