f8f00ce454c7d63aae518f891e7c6c157786c819
seabbs <s.e.abbott12@gmail.com>
2020-08-17 21:09:22 +0000

    update forecast Rt submissions
    Former-commit-id: 8e756a7f882a9158453171e6bfb544eec07ed1c6
    Former-commit-id: c9e9e8938d07f89b352e41fd173bc95e6df44c3c

--- rt-forecast-2/format/update-historic-rt-format.R
+++ rt-forecast-2/format/update-historic-rt-format.R
@@ -4,10 +4,12 @@ all_dates <- readRDS(here::here("utils", "all_dates.rds"))
 forecast_dates <- sort(as.vector(all_dates$rt_forecasts), decreasing = TRUE)
 forecast_dates <- forecast_dates[2:5]
 
+submission_dates <- sort(as.vector(all_dates$submissions), decreasing = TRUE)
+submission_dates <- submission_dates[2:5]
 # Get format function -----------------------------------------------------
 
 source(here::here("rt-forecast-2", "format", "utils", "format-rt.R"))
 
 # Format forecast ---------------------------------------------------------
 
-purrr::walk(forecast_dates, format_rt)
+purrr::walk2(forecast_dates, submission_dates, format_rt)
