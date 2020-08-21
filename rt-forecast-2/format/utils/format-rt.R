c91d4be4c188de3009a105251e151fb014f3dd17
seabbs <s.e.abbott@gmail.com>
2020-08-21 11:11:01 +0000

    add latest skip
    Former-commit-id: bb487fc78c7a6a9e16479a36c5c685f501dfb0df
    Former-commit-id: 2d106504e7020f790535086a5d58d7de50390084

--- rt-forecast-2/format/utils/format-rt.R
+++ rt-forecast-2/format/utils/format-rt.R
@@ -10,7 +10,7 @@ require(cowplot)
 require(data.table)
 
 
-format_rt <- function(forecast_date, submission_date) {
+format_rt <- function(forecast_date, submission_date, include_latest = FALSE) {
   
   rt_models <- c("original", "fixed_future_rt", "no_daily_effect", "minimal_delay", "fixed_rt")
   
@@ -74,9 +74,10 @@ format_rt <- function(forecast_date, submission_date) {
     readr::write_csv(formatted_forecasts, 
                      paste0(output_dir, "/submission-files/dated/", submission_date, "-rt-2-forecast.csv"))
     
-    readr::write_csv(formatted_forecasts, 
-                     paste0(output_dir, "/submission-files/latest.csv"))
-    
+    if (include_latest) {
+      readr::write_csv(formatted_forecasts, 
+                       paste0(output_dir, "/submission-files/latest.csv"))
+    }
     
   }
   
