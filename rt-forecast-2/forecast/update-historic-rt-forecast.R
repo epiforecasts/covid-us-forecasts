f1bceff2a23f465d75778fec5d424e56c87294ea
seabbs <s.e.abbott@gmail.com>
2020-08-24 10:15:59 +0000

    add daily effect

--- rt-forecast-2/forecast/update-historic-rt-forecast.R
+++ rt-forecast-2/forecast/update-historic-rt-forecast.R
@@ -3,7 +3,7 @@
 
 all_dates <- readRDS(here::here("utils", "all_dates.rds"))
 submission_dates <- sort(as.vector(all_dates$rt_forecasts), decreasing = TRUE)
-submission_dates <- submission_dates[2:5]
+submission_dates <- submission_dates[1:5]
 
 # Load in Rt forecast data ------------------------------------------------
 
--- rt-forecast-2/format/update-historic-rt-format.R
+++ rt-forecast-2/format/update-historic-rt-format.R
@@ -2,10 +2,10 @@
 # past forecasts ---------------------------------------------------
 all_dates <- readRDS(here::here("utils", "all_dates.rds"))
 forecast_dates <- sort(as.vector(all_dates$rt_forecasts), decreasing = TRUE)
-forecast_dates <- forecast_dates[2:5]
+forecast_dates <- forecast_dates[1:5]
 
 submission_dates <- sort(as.vector(all_dates$submissions), decreasing = TRUE)
-submission_dates <- submission_dates[2:5]
+submission_dates <- submission_dates[1:5]
 # Get format function -----------------------------------------------------
 
 source(here::here("rt-forecast-2", "format", "utils", "format-rt.R"))
