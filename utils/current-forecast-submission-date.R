e4eb2a8fa89a29182148e858e6f0b1de325cad13
seabbs <s.e.abbott12@gmail.com>
2020-09-01 14:48:24 +0000

    turn all dates back on

--- utils/current-forecast-submission-date.R
+++ utils/current-forecast-submission-date.R
@@ -1,12 +1,12 @@
 # update forecast and submission dates
 
-forecast_date <- Sys.Date()
-submission_date <- Sys.Date() # update accordingly
-# 
-# source(here::here("utils", "forecast-submission-dates.R"))
-# #
-# all_dates <- get_dates(ts_do_forecast_date = as.character(forecast_date),
-#                        ts_doc_forecast_date = as.character(forecast_date),
-#                        rt_forecast_date = as.character(forecast_date),
-#                        submission_date = as.character(submission_date),
-#                        save = TRUE)
+forecast_date <- Sys.Date() - 1
+submission_date <- Sys.Date() -1  # update accordingly
+
+source(here::here("utils", "forecast-submission-dates.R"))
+#
+all_dates <- get_dates(ts_do_forecast_date = as.character(forecast_date),
+                       ts_doc_forecast_date = as.character(forecast_date),
+                       rt_forecast_date = as.character(forecast_date),
+                       submission_date = as.character(submission_date),
+                       save = TRUE)
