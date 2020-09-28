4310616cccd05fd4fc9b2e3c1d0c40293f3c975a
seabbs <s.e.abbott12@gmail.com>
2020-09-28 21:00:45 +0000

    change shrinkage order

--- rt-forecast-2/format/update-historic-rt-format.R
+++ rt-forecast-2/format/update-historic-rt-format.R
@@ -6,10 +6,9 @@ forecast_dates <- forecast_dates[1:5]
 
 submission_dates <- sort(as.vector(all_dates$submissions), decreasing = TRUE)
 submission_dates <- submission_dates[1:5]
-# Get format function -----------------------------------------------------
 
+# Get format function -----------------------------------------------------
 source(here::here("rt-forecast-2", "format", "utils", "format-rt.R"))
 
 # Format forecast ---------------------------------------------------------
-
 purrr::walk2(forecast_dates, submission_dates, format_rt)
