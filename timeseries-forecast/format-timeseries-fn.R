81b465ab595beb5092bbefcd12532fa1e641b5b1
Kath Sherratt <62290797+kathsherratt@users.noreply.github.com>
2020-10-12 23:58:59 +0100

    Update format-timeseries-fn.R

--- timeseries-forecast/format-timeseries-fn.R
+++ timeseries-forecast/format-timeseries-fn.R
@@ -51,7 +51,7 @@ incident_forecast <- raw_weekly_forecast %>%
                                sep = " ")) %>%
   dplyr::select(forecast_date = date_created, submission_date, target, 
                 target_end_date = epiweek_end_date, 
-                type, quantile, value = deaths, location)
+                type, quantile, value = deaths, location, state)
  
 # Add point forecast
 incident_forecast <- incident_forecast %>%
