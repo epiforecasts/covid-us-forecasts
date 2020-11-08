1721022b43a406708f5b5a310ab5782f59bed793
Sam Abbott <s.e.abbott12@gmail.com>
2020-11-08 22:29:40 +0000

    Update run-rt-forecast.R

--- rt-forecast-2/forecast/utils/run-rt-forecast.R
+++ rt-forecast-2/forecast/utils/run-rt-forecast.R
@@ -8,7 +8,7 @@ run_rt_forecast <- function(deaths, submission_date, rerun = FALSE) {
     rerun <- TRUE
   }
   # Set up directories for models -------------------------------------------
-  models <- list("original", "fixed_future_rt", "fixed_rt", "no_delay")
+  models <- list("original", "fixed_future_rt", "fixed_rt", "no_delay", "backcalculation")
   
   targets <- purrr::map(models, ~ paste0("rt-forecast-2/forecast/deaths_forecast/", .x, "/state"))
   names(targets) <- models
@@ -41,12 +41,9 @@ run_rt_forecast <- function(deaths, submission_date, rerun = FALSE) {
   # Set up common settings --------------------------------------------------
   std_regional_epinow <- purrr::partial(regional_epinow, samples = 2000, horizon = 30, 
                                         generation_time = generation_time,
-                                        stan_args = list(warmup = 500, cores = no_cores, 
-                                                         control = list(adapt_delta = 0.95,
-                                                                        max_treedepth = 15), 
+                                        stan_args = list(warmup = 250, cores = no_cores, 
                                                          chains = ifelse(no_cores <= 4, 4, no_cores)), 
-                                        burn_in = 14, non_zero_points = 14,
-                                        max_execution_time = Inf, future = FALSE,
+                                        non_zero_points = 14,
                                         output = c("region", "samples", "summary", "timing"))
   # Run Rt - ORIGINAL -------------------------------------------------------
    if ("original" %in% models) {
@@ -55,7 +52,8 @@ run_rt_forecast <- function(deaths, submission_date, rerun = FALSE) {
                          summary_args = list(summary_dir = summary[["original"]],
                                              all_regions = FALSE),
                          logs = "rt-forecast-2/logs/original",
-                         delays = list(incubation_period, reporting_delay))
+                         delays = list(incubation_period, reporting_delay),
+                         gp = list(future = "project"))
    }
 
   # Run Rt - FIXED RT --------------------------------------------------
@@ -67,7 +65,7 @@ run_rt_forecast <- function(deaths, submission_date, rerun = FALSE) {
                                              all_regions = FALSE),
                          logs = "rt-forecast-2/logs/fixed_future_rt",
                          delays = list(incubation_period, reporting_delay),
-                         future_rt = "latest")
+                         gp = list(future = "latest"))
    }
 
   
@@ -81,7 +79,7 @@ run_rt_forecast <- function(deaths, submission_date, rerun = FALSE) {
                                             all_regions = FALSE),
                         logs = "rt-forecast-2/logs/fixed_rt",
                         delays = list(incubation_period, reporting_delay),
-                       future_rt = "estimate")
+                        gp = list(future = "estimate"))
   }  
   
 
@@ -94,6 +92,17 @@ run_rt_forecast <- function(deaths, submission_date, rerun = FALSE) {
                                             all_regions = FALSE),
                         logs = "rt-forecast-2/logs/no_delay",
                         future_rt = "latest")
+  }
+  
+  # Backcalculation ----------------------------------------------------------------
+  
+  if ("backcalculation" %in% models) {
+    std_regional_epinow(reported_cases = deaths,
+                        target_folder = targets[["no_delay"]],
+                        summary_args = list(summary_dir = summary[["no_delay"]],
+                                            all_regions = FALSE),
+                        rt = NULL,
+                        logs = "rt-forecast-2/logs/no_delay")
   }
   
   
