c6ab93661e8a559ae4cadecb81e4b655b7559857
kathsherratt <katharine.sherratt@lshtm.ac.uk>
2020-11-26 21:56:47 +0000

    add population argument to rt_opts
    Added state populations to EpiNow2 `rt_opts`
    - set up a standard `rt_opts()` list
    - import state populations and add to rt_opts(pop)
    - standardise rt_opts(prior) - previously this was identical repeated in each model args
    - rearrange model args
       - use the standard rt_opts for pop & prior
       - set model-specific rt_opts(future)

--- rt-forecast-2/forecast/utils/run-rt-forecast.R
+++ rt-forecast-2/forecast/utils/run-rt-forecast.R
@@ -30,20 +30,29 @@ run_rt_forecast <- function(deaths,
     
     models <- models[!targets_present]
   }
-  
-  if (length(models) == 0) {
-    return(invisible(NULL))
-  }
 
   # Format for epinow2 ------------------------------------------------------
   
+  # Deaths data
   if (!missing(submission_date)) {
     deaths <- dplyr::filter(deaths, date < submission_date)
   }
-  
   deaths <- setDT(deaths)
   deaths <- deaths[, .SD[date >= (max(date) - lubridate::weeks(8))], by = region]
   data.table::setorder(deaths, date)
+  
+  # Rt global options
+  rt <- opts_list(rt_opts(), deaths) %>%
+    purrr::transpose()
+  
+  # - Population data
+  rt$pop <- suppressMessages(readr::read_csv("utils/state_pop_totals.csv")) %>%
+    dplyr::pull(tot_pop, name = state_name) %>%
+    as.list()
+  
+  # - Prior
+  rt$prior <- purrr::map(rt$prior, ~ list(mean = 1, sd = 0.2))
+  
   
   # Set up common settings --------------------------------------------------
 
@@ -55,30 +64,41 @@ run_rt_forecast <- function(deaths,
                                                          warmup = 250, 
                                                          cores = no_cores, 
                                                          chains = ifelse(no_cores <= 4, 4, no_cores)),
-                                        output = c("region", "samples", "summary", "timing"))
+                                        output = c("region", "samples", "summary", "timing"),
+                                        obs = obs_opts(scale = list(mean = 0.01, sd = 0.001)))
   # Run Rt - ORIGINAL -------------------------------------------------------
    if ("original" %in% models) {
+     # Rt options
+     rt_original <- rt
+     rt_original$future <- purrr::map(rt_original$future, ~ "project")
+     rt_original <- purrr::transpose(rt_original)
+     
+     # Run model
      std_regional_epinow(reported_cases = deaths,
                          target_folder = targets[["original"]],
                          summary_args = list(summary_dir = summary[["original"]],
                                              all_regions = FALSE),
                          logs = "rt-forecast-2/logs/original",
                          delays = delay_opts(incubation_period, reporting_delay),
-                         rt = rt_opts(prior = list(mean = 1, sd = 0.2), 
-                                   future = "project"))
+                         rt = rt_original)
    }
 
   # Run Rt - FIXED RT --------------------------------------------------
 
    if ("fixed_future_rt" %in% models) {
+     # Rt options
+     rt_fixed_future <- rt
+     rt_fixed_future$future <- purrr::map(rt_fixed_future$future, ~ "latest")
+     rt_fixed_future <- purrr::transpose(rt_fixed_future)
+     
+     # Run model
      std_regional_epinow(reported_cases = deaths,
                          target_folder = targets[["fixed_future_rt"]],
                          summary_args = list(summary_dir = summary[["fixed_future_rt"]],
                                              all_regions = FALSE),
                          logs = "rt-forecast-2/logs/fixed_future_rt",
                          delays = delay_opts(incubation_period, reporting_delay),
-                         rt = rt_opts(prior = list(mean = 1, sd = 0.2), 
-                                   future = "latest"))
+                         rt = rt_fixed_future)
    }
 
   
@@ -86,32 +106,38 @@ run_rt_forecast <- function(deaths,
 # Fixed Rt ----------------------------------------------------------------
 
   if ("fixed_rt" %in% models) {
+    # Rt options
+    rt_fixed <- rt
+    rt_fixed$future <- purrr::map(rt_fixed$future, ~ "estimate")
+    rt_fixed <- purrr::transpose(rt_fixed)
+    
+    # Run model
     std_regional_epinow(reported_cases = deaths,
                         target_folder = targets[["fixed_rt"]],
                         summary_args = list(summary_dir = summary[["fixed_rt"]],
                                             all_regions = FALSE),
                         logs = "rt-forecast-2/logs/fixed_rt",
                         delays = delay_opts(incubation_period, reporting_delay),
-                         rt = rt_opts(prior = list(mean = 1, sd = 0.2), 
-                                   future = "estimate"))
+                        rt = rt_fixed)
   }  
   
 
 # No delay ----------------------------------------------------------------
   
   if ("no_delay" %in% models) {
+    # Run model
     std_regional_epinow(reported_cases = deaths,
                         target_folder = targets[["no_delay"]],
                         summary_args = list(summary_dir = summary[["no_delay"]],
                                             all_regions = FALSE),
                         logs = "rt-forecast-2/logs/no_delay",
-                         rt = rt_opts(prior = list(mean = 1, sd = 0.2), 
-                                   future = "latest"))
+                         rt = purrr::transpose(rt))
   }
   
   # Backcalculation ----------------------------------------------------------------
   
   if ("backcalc" %in% models) {
+    # Run model
     std_regional_epinow(reported_cases = deaths,
                         target_folder = targets[["backcalc"]],
                         summary_args = list(summary_dir = summary[["backcalc"]],
