67ade74a127eb8a09472962feca5d238a279f560
SophieMeakin <sophiemeakin@outlook.com>
2020-12-14 18:52:29 +0000

    partial timeseries adaption

--- models/timeseries/utils/deaths-on-cases-forecast.R
+++ models/timeseries/utils/deaths-on-cases-forecast.R
@@ -17,128 +17,79 @@
 # quantiles_out <- c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99)
 
 library(magrittr); library(dplyr); library(tidyr); library(EpiSoon); library(forecastHybrid)
+library(lubridate);
 
 deaths_on_cases_forecast <- function(case_data, deaths_data, case_quantile,
-                                     sample_count, horizon_weeks, right_truncate_weeks){
-    
-
-# Set up cases ----------------------------------------------------------
-
-  # Set up case data and truncate
-  case_data_weekly_full <- case_data %>%
-    mutate(epiweek = lubridate::epiweek(date),
-           date = NULL) %>%
-    group_by(state, epiweek) %>%
-    summarise(cases = sum(cases), .groups = "drop_last") 
+                                     sample_count, window = 6, horizon = 30){
+# Set up observations ----------------------------------------------------------
+  case_data <- case_data %>% 
+    group_by(state) %>% 
+    filter(date >= max(date) - weeks(window + 3)) %>% 
+    ungroup()
   
-  right_truncate_date <- max(case_data_weekly_full$epiweek) - right_truncate_weeks
+  deaths_data <- deaths_data %>% 
+    group_by(state) %>% 
+    filter(date >= max(date) - weeks(window)) %>% 
+    ungroup()
   
-  case_data_weekly <- case_data_weekly_full %>%
-    filter(epiweek <= right_truncate_date) 
-  
-
-# Set up dates -----------------------------------------------------------
-
-  # Forecasting = next week + horizon_weeks
-  forecast_weeks <- seq(from = max(case_data_weekly$epiweek)+1, by = 1, length.out = horizon_weeks)
-  case_forecast_weeks <- seq(from = max(case_data_weekly$epiweek)+1, by = 1, length.out = horizon_weeks+2)
-  
-  # Historical = last 6 weeks of data
-  historical_weeks <- case_data_weekly %>%
-    ungroup() %>%
-    filter(!epiweek %in% forecast_weeks) %>%
-    select(epiweek) %>%
-    filter(epiweek > max(epiweek) - 6) %>%
-    unique() %>%
-    pull(epiweek)
-
-  
-
 # Forecast cases ---------------------------------------------------------
-    
-  case_forecast <- suppressMessages(case_data_weekly %>%
+  case_forecast <- suppressMessages(case_data %>%
       group_by(state) %>%
-      group_modify(~ EpiSoon::forecastHybrid_model(y = filter(.x, epiweek %in% historical_weeks) %>%
-                                                     pull("cases"),
-                                                   samples = sample_count, 
-                                                   horizon = horizon_weeks+2,
+      group_modify(~ EpiSoon::forecastHybrid_model(y = .x$cases,
+                                                   samples = 1, 
+                                                   horizon = horizon,
                                                    model_params = list(models = "aez", weights = "equal",
                                                                        a.args = list()),
                                                    forecast_params = list(PI.combination = "mean"))) %>%
-      mutate(sample = rep(1:sample_count)) %>%
-      tidyr::pivot_longer(cols = starts_with("..."), names_to = "epiweek"))
-    
-    # Get quantile
-      quantile <- case_forecast %>%
-        group_by(state, epiweek) %>%
-        group_modify( ~ as.data.frame(quantile(.x$value, probs = case_quantile, na.rm = T))) %>%
-        mutate(quantile = case_quantile) %>%
-        ungroup()
-      
-      # Format
-      case_dates_from <- unique(quantile$epiweek)[1:length(case_forecast_weeks)]
-       
-      case_forecast <- quantile %>%
-        select(state, epiweek, "cases" = 3) %>%
-        mutate(cases = ifelse(cases < 0, 0, cases),
-               cases = round(cases)) %>%
-        mutate(epiweek = recode(epiweek, !!! setNames(case_forecast_weeks, case_dates_from)))
-
-      
+      tidyr::pivot_longer(cols = starts_with("X"), names_to = "day")) %>% 
+      mutate(day = day %>% 
+               stringr::str_remove_all("X") %>% 
+               as.numeric()) %>% 
+      mutate(date = max(case_data$date) + day, cases = value) %>% 
+      select(state, date, cases)
 
 # Forecast deaths ---------------------------------------------------------
       
-      # Set deaths data and truncate
-      deaths_data_weekly_full <- deaths_data %>%
-        mutate(epiweek = lubridate::epiweek(date),
-               date = NULL) %>%
-        group_by(state, epiweek) %>%
-        summarise(deaths = sum(deaths), .groups = "drop_last")
-      
-      deaths_data_weekly <- deaths_data_weekly_full %>%
-        filter(epiweek <= right_truncate_date)
-      
       # Join deaths and cases
-      cases_deaths <- dplyr::bind_rows(case_data_weekly, case_forecast) %>%
+      cases_deaths <- dplyr::bind_rows(case_data %>% 
+                                         select(-epiweek), case_forecast) %>%
+        arrange(date) %>% 
         group_by(state) %>%
-        mutate(cases_lag1 = dplyr::lag(cases, 1),
-               cases_lag2 = dplyr::lag(cases, 2)) %>%
-        filter(epiweek <= max(epiweek)-2) %>%
+        mutate(cases_lag1 = dplyr::lag(cases, 7),
+               cases_lag2 = dplyr::lag(cases, 14)) %>%
+    
+        mutate(cases = data.table::frollsum(cases, 7),
+               cases_lag1 = data.table::frollsum(cases_lag1, 7),
+               cases_lag2 = data.table::frollsum(cases_lag2, 7)) %>%
+        filter(date >= min(deaths_data$date)) %>% 
         ungroup() %>%
-        full_join(deaths_data_weekly, by = c("state", "epiweek"))
+        drop_na() %>% 
+        arrange(state) %>% 
+        full_join(deaths_data, by = c("state", "date"))
       
       # Forecast deaths (y) using cases
       death_forecast <- suppressMessages(cases_deaths %>%
         group_by(state) %>%
-        group_modify(~ EpiSoon::forecastHybrid_model(y = filter(.x, epiweek %in% historical_weeks) %>%
-                                                       pull("deaths"),
+        group_modify(~ EpiSoon::forecastHybrid_model(y = .x$deaths,
                                                      samples = sample_count, 
-                                                     horizon = horizon_weeks,
+                                                     horizon = horizon,
                                                      model_params = list(models = "aez", weights = "equal",
                                                                          a.args = list(
                                                                          xreg = 
                                                                          as.matrix(
-                                                                           filter(.x, epiweek %in% historical_weeks) %>%
-                                                                           pull("cases"),
-                                                                         filter(.x, epiweek %in% historical_weeks) %>%
-                                                                           pull("cases_lag1"),
-                                                                         filter(.x, epiweek %in% historical_weeks) %>%
-                                                                           pull("cases_lag2")))
-                                                                         ),
+                                                                           .x$cases,
+                                                                           .x$cases_lag1,
+                                                                           .x$cases_lag2)
+                                                                         )),
                                                      forecast_params = list(xreg = 
-                                                                            as.matrix(
-                                                                            filter(.x, epiweek %in% forecast_weeks) %>%
-                                                                              pull("cases"),
-                                                                            filter(.x, epiweek %in% forecast_weeks) %>%
-                                                                              pull("cases_lag1"),
-                                                                            filter(.x, epiweek %in% forecast_weeks) %>%
-                                                                              pull("cases_lag2")),
-                                                                            PI.combination = "mean")
+                                                                              as.matrix(
+                                                                                .x$cases,
+                                                                                .x$cases_lag1,
+                                                                                .x$cases_lag2),
+                                                                            PI.combination = "mean"))
                                                      )) %>%
         mutate(sample = rep(1:sample_count)) %>%
-        tidyr::pivot_longer(cols = starts_with("..."), names_to = "epiweek"))
-      
-      
+        tidyr::pivot_longer(cols = starts_with("..."), names_to = "day")
 
 # Return forecast ---------------------------------------------------------
 
