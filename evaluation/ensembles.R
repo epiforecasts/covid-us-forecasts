9d9b8d538bcef16b718be35b99639356d4f07ab3
seabbs <s.e.abbott12@gmail.com>
2020-12-16 22:20:01 +0000

    check

--- evaluation/ensembles.R
+++ evaluation/ensembles.R
@@ -17,10 +17,11 @@ forecasts <- load_submissions(target_date, "ensembles", summarise = FALSE)
 
 # Plot forecasts ----------------------------------------------------------
 source(here("evaluation", "utils", "plot_forecast.R"))
-source(here("evaluation", "utils", "summarise_submissions.R"))
+source(here("utils", "summarise_submissions.R"))
 
 # forecasts to plot
 plotted_forecasts <- copy(forecasts)[(window == 4 & horizons == "1, 2, 3, 4") | (is.na(window) & horizons == "")]
+plotted_forecasts <- plotted_forecasts[model %in% c("mean", "median", "QRA (weighted quantiles)")]
 plotted_forecasts <- summarise_submissions(plotted_forecasts)
 
 # plot usa 
