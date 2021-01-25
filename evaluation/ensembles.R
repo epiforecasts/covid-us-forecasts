144025b128ff2091bc809f949045fc45ae6ccf43
kathsherratt <kathsherratt@github.com>
2021-01-25 14:26:29 +0000

    plots have 12 weeks of data

--- evaluation/ensembles.R
+++ evaluation/ensembles.R
@@ -26,7 +26,7 @@ plotted_forecasts <- summarise_submissions(plotted_forecasts)
 
 # plot usa 
 us <- plot_forecast(plotted_forecasts[state == "US"], 
-                    obs[state == "US" & date < target_date & date > (target_date - 7*6)])
+                    obs[state == "US" & date < target_date & date > (target_date - 7*12)])
 
 source(here("utils", "check_dir.R"))
 plot_dir <- here("evaluation", "plots", target_date, "ensembles")
@@ -35,5 +35,5 @@ ggsave(paste0(plot_dir, "/us.png"), us, height = 7, width = 7)
 
 # plot states
 states <- plot_forecast(plotted_forecasts[state != "US"], 
-                        obs[state != "US" & date < target_date & date > (target_date - 7*6)])
+                        obs[state != "US" & date < target_date & date > (target_date - 7*12)])
 ggsave(paste0(plot_dir, "/states.png"), states, height = 48, width = 48)
--- evaluation/models.R
+++ evaluation/models.R
@@ -20,7 +20,7 @@ source(here("evaluation", "utils", "plot_forecast.R"))
 
 # plot usa 
 us <- plot_forecast(forecasts[state == "US"], 
-                    obs[state == "US" & date < target_date & date > (target_date - 7*6)])
+                    obs[state == "US" & date < target_date & date > (target_date - 7*12)])
 
 source(here("utils", "check_dir.R"))
 plot_dir <- here("evaluation", "plots", target_date, "models")
@@ -29,5 +29,5 @@ ggsave(paste0(plot_dir, "/us.png"), us, height = 7, width = 7)
 
 # plot states
 states <- plot_forecast(forecasts[state != "US"], 
-                        obs[state != "US" & date < target_date & date > (target_date - 7*6)])
+                        obs[state != "US" & date < target_date & date > (target_date - 7*12)])
 ggsave(paste0(plot_dir, "/states.png"), states, height = 48, width = 48)
