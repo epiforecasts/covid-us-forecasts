654ea002e09efa83e41eb99be54f0dec380b4127
nikosbosse <nikosbosse@gmail.com>
2021-02-15 11:41:00 +0100

    temporarily change update script

--- models/update.sh
+++ models/update.sh
@@ -1,16 +1,16 @@
 #!bin/bash
 
 # update constant baseline model
-bash models/constant-baseline/update.sh
+Rscript models/constant-baseline/update-constant-baseline.R
 
 # update rt deaths forecast
-bash models/rt/update.sh
+# bash models/rt/update.sh
 
 # update deaths as a convolution of cases model
-bash models/deaths-conv-cases/update.sh
+# bash models/deaths-conv-cases/update.sh
 
 # update timeseries model
-bash models/timeseries/update.sh
+# bash models/timeseries/update.sh
 
 # update aggregation of all models (for ensembling)
 Rscript models/aggregate-for-submission.R
