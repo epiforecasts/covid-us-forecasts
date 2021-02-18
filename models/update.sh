6aa49ed995644e05872debe1b71a2201c6f48e7f
nikosbosse <nikosbosse@gmail.com>
2021-02-18 13:26:09 +0100

    change update script back to update all models

--- models/update.sh
+++ models/update.sh
@@ -4,13 +4,13 @@
 Rscript models/constant-baseline/update-constant-baseline.R
 
 # update rt deaths forecast
-# bash models/rt/update.sh
+bash models/rt/update.sh
 
 # update deaths as a convolution of cases model
-# bash models/deaths-conv-cases/update.sh
+bash models/deaths-conv-cases/update.sh
 
 # update timeseries model
-# bash models/timeseries/update.sh
+bash models/timeseries/update.sh
 
 # update aggregation of all models (for ensembling)
 Rscript models/aggregate-for-submission.R
