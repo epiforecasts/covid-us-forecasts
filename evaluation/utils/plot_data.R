0c6d5c537c953eb7f1be672ae5ae40332d8a0441
kathsherratt <kathsherratt@gmail.com>
2021-02-22 22:23:17 +0000

    update plots

--- evaluation/utils/plot_data.R
+++ evaluation/utils/plot_data.R
@@ -3,14 +3,14 @@ library(ggplot2)
 library(cowplot)
 library(dplyr)
 
-plot_data <- function(weekly, daily){
-plot <- weekly %>%
+plot_data <- function(data){
+plot <- data %>%
     mutate(state_location = paste0(state, " (", location, ")")) %>%
-    ggplot(aes(x = date, y = value, fill = NULL)) +
-    geom_point(col = "black") +
-    geom_line(col = "black") +
-    geom_point(data = daily, col = "dark grey", size = 0.5) +
-    facet_wrap(.~ state_location, scales = "free_y") +
+    ggplot(aes(x = date, fill = NULL)) +
+    geom_point(aes(y = weekly), col = "black") +
+    geom_line(aes(y = weekly), col = "grey10") +
+    geom_point(aes(y = daily), col = "dark grey", size = 0.5) +
+    facet_wrap(~ state_location, scales = "free_y") +
     ggplot2::labs(x = "Week ending", y = "Weekly incident deaths", 
                   col = "Model", fill = "Model") +
     theme_cowplot() +
