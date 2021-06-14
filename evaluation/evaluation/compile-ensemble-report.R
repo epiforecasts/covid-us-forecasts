fab905306c230c049a2c4935d8310d98e6351a4c
kathsherratt <kathsherratt@gmail.com>
2021-06-14 21:52:21 +0100

    add code from ecdc hub

---
+++ evaluation/evaluation/compile-ensemble-report.R
@@ -0,0 +1,29 @@
+# packages ---------------------------------------------------------------------
+library(dplyr)
+library(here)
+library(readr)
+library(scoringutils)
+library(rmarkdown)
+library(data.table)
+library(covidHubUtils)
+library(lubridate)
+
+options(knitr.duplicate.label = "allow")
+
+report_date <-
+  lubridate::floor_date(lubridate::today(), "week", week_start = 1)
+
+dir.create(here::here("html"))
+
+rmarkdown::render(here::here("evaluation", "evaluation",
+                             "ensemble-report.Rmd"),
+                  params = list(report_date = report_date,
+                                restrict_weeks = 4),
+                  output_format = "html_document",
+                  output_file =
+                    here::here("html", paste0("ensemble-report.html")),
+                  envir = new.env())
+
+## to make this generalisable
+# allow bits to be turned off and on
+# somehow pass down the filtering
