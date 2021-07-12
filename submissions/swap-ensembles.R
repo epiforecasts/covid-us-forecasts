8383d1ea8ffae959b2987fb21931782fdb9426cc
kathsherratt <kathsherratt@gmail.com>
2021-07-12 22:42:30 +0100

    unsuccessful qra; replace with median

--- submissions/swap-ensembles.R
+++ submissions/swap-ensembles.R
@@ -2,20 +2,24 @@
 library(here)
 
 target_date <- as.Date(readRDS(here("data", "target_date.rds")))
+state_codes <- readRDS("C:/Users/kaths/GitHub/covid-us-forecasts/data/state_codes.rds")
 
 # Add state - model combinations here
 swap <- list(
   # "mean" = c(
   #
   # ),
+  "median" = c(
+    state_codes$state
+  )
   # 
   # "Timeseries" = c(
   # 
   # ),
   # 
-  "Rt" = c(
-    "Florida"
-  )
+  # "Rt" = c(
+  #   "Florida"
+  # )
   #,
   # "Case convolution" = c(
   # 
